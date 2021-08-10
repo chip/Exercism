a6ec4d42270dfd19ab198aa3a491943e849a9406
Chip Castle <chip@chipcastle.com>
2021-08-09 19:32:24 -0500

    Remove cruft from beer-song exercise (Exercism/clojure)

--- clojure/beer-song/src/beer_song.clj
+++ clojure/beer-song/src/beer_song.clj
@@ -3,7 +3,6 @@
 
 (defn last-verse
   []
-  ; (println "last-verse")
   "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
 
 (defn bottles
@@ -11,6 +10,7 @@
   (format "%s %s" (if (= num 0) "no more" num) (if (= num 1) "bottle" "bottles")))
 
 (defn of-beer [] "of beer")
+
 (defn on-the-wall [] "on the wall")
 (defn of-beer-on-the-wall [] (format "%s %s" (of-beer) (on-the-wall)))
 
@@ -25,7 +25,6 @@
 
 (defn compose-verse
   [num]
-  ; (println (format "compose-verse %s" num))
   (format "%s.\n%s %s %s.\n" 
                   (verse-part-1 num)
                   (take-one-down num)
@@ -35,7 +34,6 @@
 (defn verse
   "Returns the nth verse of the song."
   [num]
-  ; (println (format "verse %s" num))
   (if (= num 0)
     (last-verse)
     (compose-verse num)))
@@ -45,32 +43,3 @@
     (sing start 0))
   ([start end]
    (str/join "\n" (map #(beer-song/verse %) (range start (dec end) -1)))))
-
-; (defn sing
-;   "Given a start and an optional end, returns all verses in this interval. If end is not given, the whole song from start is sung."
-;   ([start]
-;     (sing start 0))
-;   ([start end]
-;     (loop [current start]
-;       ; (println (format "loop %s %s" current end))
-;       (when (>= current end)
-;         ; (println (format "current %s" current))
-;         (verse current)
-;         (recur (- current 1))))))
-
-; (defn sing
-;   "Given a start and an optional end, returns all verses in this interval. If end is not given, the whole song from start is sung."
-;   ([start]
-;     (sing start 0))
-;   ([start end]
-;     (println (format "sing %s %s" start end))
-;     (if (> start end)
-;       (verse start)
-;       (sing (dec start) end))))
-
-(defn -main
-  []
-  (sing 8 6)
-  (sing 3))
-
-; (-main)
