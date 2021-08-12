2bc47210f655f83c8b0e442a8fd3431c92bc5982
Chip Castle <chip@chipcastle.com>
2021-08-11 22:02:58 -0500

    Add nucleotide-count solution (tests all green)

--- clojure/nucleotide-count/src/nucleotide_count.clj
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -1,10 +1,17 @@
-(ns nucleotide-count)
+(ns nucleotide-count
+  (:require
+    [clojure.string :as str]))
 
-(defn count-of-nucleotide-in-strand [nucleotide strand] ;; <- Arglist goes here
-  ;; your code goes here
-)
+(defn dna-alphabet [] (list \A \C \G \T))
 
+(defn count-of-nucleotide-in-strand [nucleotide strand]
+  (if (not (some #(= nucleotide %) (dna-alphabet)))
+    (throw (Exception. (format "Nucleotide %s does not exist" nucleotide)))
+    (if (str/blank? strand)
+      0
+      (count (filterv #(= (str nucleotide) (str %)) (re-seq #"." strand))))))
 
-(defn nucleotide-counts [strand] ;; <- Arglist goes here
-  ;; your code goes here
-)
+(defn nucleotide-counts [strand]
+  (if (str/blank? strand)
+    (into {} (map #(hash-map % 0) (dna-alphabet)))
+    (into {} (map #(hash-map % (count-of-nucleotide-in-strand % strand)) (dna-alphabet)))))
