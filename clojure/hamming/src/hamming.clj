fcb259803ea5a3ebca2402cb140c12b40983cb33
Chip Castle <chip@chipcastle.com>
2021-08-14 21:06:03 -0500

    Add solution for hamming

--- clojure/hamming/src/hamming.clj
+++ clojure/hamming/src/hamming.clj
@@ -1,5 +1,16 @@
 (ns hamming)
 
-(defn distance [strand1 strand2] ; <- arglist goes here
-  ;; your code goes here
-)
+(defn hamming-occurs? [strand1 strand2 i]
+  (let [c1 (.charAt strand1 i)
+        c2 (.charAt strand2 i)
+        occurs (not (= c1 c2))
+        ]
+    occurs))
+
+(defn indexes [n] (range n))
+
+(defn distance [strand1 strand2]
+  (if (= strand1 strand2)
+    0
+    (if (= (count strand1) (count strand2))
+      (count (filter identity (map #(hamming-occurs? strand1 strand2 %) (indexes (count strand1))))))))
