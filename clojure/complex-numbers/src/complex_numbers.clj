593a22480afdc54b990a58d59cb96e405e5f3937
Chip Castle <chip@chipcastle.com>
2021-08-16 15:30:37 -0500

    Add solution for complex-numbers (side exercise)

--- clojure/complex-numbers/src/complex_numbers.clj
+++ clojure/complex-numbers/src/complex_numbers.clj
@@ -1,33 +1,22 @@
 (ns complex-numbers)
 
-(defn real [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn real [[a b]] a)
 
-(defn imaginary [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn imaginary [[a b]] b)
 
-(defn abs [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn abs [[a b]] (Math/sqrt (+ (Math/pow a 2) (Math/pow b 2))))
 
-(defn conjugate [[a b]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn conjugate [[a b]] [a (* b -1)])
 
-(defn add [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn add [[a b] [c d]] [(+ a c) (+ b d)])
 
-(defn sub [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn sub [[a b] [c d]] [(- a c) (- b d)])
 
-(defn mul [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn mul [[a b] [c d]] [(- (* a c) (* b d)) (+ (* b c) (* a d))])
 
-(defn div [[a b] [c d]] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn div [[a b] [c d]]
+  (let [c2 (Math/pow c 2)
+        d2 (Math/pow d 2)
+        real-part (/ (+ (* a c) (* b d)) (+ c2 d2))
+        imaginary-part (/ (- (* b c) (* a d)) (+ c2 d2))]
+      [real-part imaginary-part]))
