96fa80b52938ee9953c1fe150917c87e5555e430
Chip Castle <chip@chipcastle.com>
2021-08-19 11:07:27 -0500

    Update bob solution based on mentor's feedback

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,13 +1,23 @@
 (ns bob
   (:require [clojure.string :as str]))
 
-(defn nothing-said? [s] (str/blank? s))
-(defn ends-with-question? [s] (re-find #"\?$" s))
-(defn has-lower-case? [s] (re-find #"[a-z]+" s))
-(defn has-upper-case? [s] (re-find #"[A-Z]+" s))
-(defn shouting? [s] (and (has-upper-case? s) (not (has-lower-case? s))))
-(defn contains-letters? [s] (re-find #"[a-zA-Z]+" s))
-(defn shouting-questions? [s] (and (ends-with-question? s) (shouting? s) (contains-letters? s)))
+(defn nothing-said? [s]
+  (str/blank? s))
+
+(defn ends-with-question? [s]
+  (re-find #"\?$" s))
+
+(defn has-lower-case? [s]
+  (re-find #"[a-z]+" s))
+
+(defn has-upper-case? [s]
+  (re-find #"[A-Z]+" s))
+
+(defn shouting? [s]
+  (and (has-upper-case? s) (not (has-lower-case? s))))
+
+(defn shouting-questions? [s]
+  (and (ends-with-question? s) (shouting? s)))
 
 (defn response-for [str]
   (let [s (str/trim str)]
