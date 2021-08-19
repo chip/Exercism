0ac082ea653bc2327219ad9b3f724381a6206129
Chip Castle <chip@chipcastle.com>
2021-08-19 11:03:53 -0500

    Refactor string trim

--- clojure/bob/src/bob.clj
+++ clojure/bob/src/bob.clj
@@ -1,18 +1,19 @@
 (ns bob
   (:require [clojure.string :as str]))
 
-(defn nothing-said? [s] (str/blank? (str/trim s)))
-(defn ends-with-question? [s] (re-find #"\?$" (str/trim s)))
+(defn nothing-said? [s] (str/blank? s))
+(defn ends-with-question? [s] (re-find #"\?$" s))
 (defn has-lower-case? [s] (re-find #"[a-z]+" s))
 (defn has-upper-case? [s] (re-find #"[A-Z]+" s))
 (defn shouting? [s] (and (has-upper-case? s) (not (has-lower-case? s))))
 (defn contains-letters? [s] (re-find #"[a-zA-Z]+" s))
 (defn shouting-questions? [s] (and (ends-with-question? s) (shouting? s) (contains-letters? s)))
 
-(defn response-for [s]
-  (cond
-    (nothing-said? s) "Fine. Be that way!"
-    (shouting-questions? s) "Calm down, I know what I'm doing!"
-    (shouting? s) "Whoa, chill out!"
-    (ends-with-question? s) "Sure."
-    :else "Whatever."))
+(defn response-for [str]
+  (let [s (str/trim str)]
+    (cond
+      (nothing-said? s) "Fine. Be that way!"
+      (shouting-questions? s) "Calm down, I know what I'm doing!"
+      (shouting? s) "Whoa, chill out!"
+      (ends-with-question? s) "Sure."
+      :else "Whatever.")))
