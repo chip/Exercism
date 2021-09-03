ae54fc2905b394e10d8dc885d5fd67e4d7b34a8d
Chip Castle <chip@chipcastle.com>
2021-09-03 18:19:34 -0500

    [Lua] Refactor house exercise

--- lua/house/house.lua
+++ lua/house/house.lua
@@ -1,39 +1,31 @@
 local house = {}
 
 house.phrases = {
-  [[]],
-  [[the malt
-that lay in]],
-  [[the rat
-that ate]],
-  [[the cat
-that killed]],
-  [[the dog
-that worried]],
-  [[the cow with the crumpled horn
-that tossed]],
-  [[the maiden all forlorn
-that milked]],
-  [[the man all tattered and torn
-that kissed]],
-  [[the priest all shaven and shorn
-that married]],
-  [[the rooster that crowed in the morn
-that woke]],
-  [[the farmer sowing his corn
-that kept]],
-  [[the horse and the hound and the horn
-that belonged to]],
+  { noun = 'house', verb = 'Jack built.' },
+  { noun = 'malt', verb = 'lay in' },
+  { noun = 'rat', verb = 'ate' },
+  { noun = 'cat', verb = 'killed' },
+  { noun = 'dog', verb = 'worried' },
+  { noun = 'cow with the crumpled horn', verb = 'tossed' },
+  { noun = 'maiden all forlorn', verb = 'milked' },
+  { noun = 'man all tattered and torn', verb = 'kissed' },
+  { noun = 'priest all shaven and shorn', verb = 'married' },
+  { noun = 'rooster that crowed in the morn', verb = 'woke' },
+  { noun = 'farmer sowing his corn', verb = 'kept' },
+  { noun = 'horse and the hound and the horn', verb = 'belonged to' },
 }
 
 house.verse = function(which)
   local buffer = {}
-
   for i=which,1,-1 do
-    table.insert(buffer, house.phrases[i])
+    local phrase = house.phrases[i]
+    local separator = '\n'
+    if i == 1 then
+      separator = ' '
+    end
+    table.insert(buffer, 'the '..phrase.noun..separator..'that '..phrase.verb)
   end
-  local result = 'This is '..table.concat(buffer, ' ')..'the house that Jack built.'
-  return result
+  return 'This is '..table.concat(buffer, ' ')
 end
 
 house.recite = function()
