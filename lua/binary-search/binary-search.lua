04593012245b1bcfc2d8042e6d78a4875f355e3d
Chip Castle <chip@chipcastle.com>
2021-09-04 18:18:39 -0500

    [Lua] Add refactored solution for binary-search

--- lua/binary-search/binary-search.lua
+++ lua/binary-search/binary-search.lua
@@ -1,13 +1,9 @@
 return function(array, target)
-  print("\n\ntarget:", target)
-
   local size = #array
-  print("size:", size)
   local first = array[1]
   local last = array[size]
 
   if size == 0 or (size == 1 and array[1] ~= target) or target > array[size] or target < array[1] then
-  -- if size == 0 or (size == 1 and first ~= target) then
     return -1
   end
 
@@ -28,9 +24,7 @@ return function(array, target)
   local right = size
 
   while left < right do
-    print("index:", index)
     if array[index] == target then
-      print("return index "..index)
       break
     elseif array[index] > target then
       index = math.ceil(index / 2)
