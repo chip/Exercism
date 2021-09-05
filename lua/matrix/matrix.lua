6e581ba32f308d754d486e825cec26e9c06bfa84
Chip Castle <chip@chipcastle.com>
2021-09-05 18:02:05 -0500

    [Lua] Refactor solution to matrix exercise

--- lua/matrix/matrix.lua
+++ lua/matrix/matrix.lua
@@ -1,65 +1,35 @@
-local inspect = require('inspect')
-
 return function(s)
-  print("s:")
-  print(s)
   local mtx = {}
-
   local i=1
-  -- local j=1
 
   for line in string.gmatch(s, "[^\n]+") do
-    print('\n')
-    print('line:'..line)
-    print('i:'..i)
-    -- local r = {}
     mtx[i] = {}
-    -- mtx[i][j] = {}
-    print(inspect(mtx))
     local j=1
 
     for element in string.gmatch(line, "[^%s]+") do
-      print('element:'..element.."*")
-      -- mtx[i][j] = element
       mtx[i][j] = element
       j = j + 1
-      -- require 'pl.pretty'.dump(mtx)
-      print(inspect(mtx))
     end
     i = i + 1
   end
-  print(inspect(mtx))
-  -- require 'pl.pretty'.dump(mtx)
 
   local function row(n)
-    print("row n:"..n)
-    -- table.sort(mtx)
-    -- require 'pl.pretty'.dump(mtx)
-    print("inspect mtx")
-    print(inspect(mtx))
     local t = {}
 
-    for k,v in ipairs(mtx[n]) do
-      print('v = ')
-      print(v)
+    for k,v in pairs(mtx[n]) do
       t[k] = tonumber(v)
-      -- table.insert(t, tonumber(v))
     end
-    print("inspect t")
-    print(inspect(t))
+
     return t
   end
 
   local function column(n)
-    print("\n")
-    print("column n:"..n)
     local t = {}
 
-    -- for k,v in pairs(mtx[n]) do
     for m=1,#mtx do
-      -- table.insert(t, tonumber(v))
       t[m] = tonumber(mtx[m][n])
     end
+
     return t
   end
 
