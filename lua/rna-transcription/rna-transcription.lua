0e7a9b5a05491db3d7d21515540ac52aad5c02fc
Chip Castle <chip@chipcastle.com>
2021-09-06 14:06:39 -0500

    [Lua] Add solution for rna-transcription

--- lua/rna-transcription/rna-transcription.lua
+++ lua/rna-transcription/rna-transcription.lua
@@ -1,3 +1,12 @@
 return function(dna)
+  local mapping = {G = 'C', C = 'G', T = 'A', A = 'U'}
+  local converted = ''
 
+  for i=1,#dna do
+    local nucleotide = dna:sub(i,i)
+    if nucleotide:match("[GCTA]") then
+      converted = converted .. mapping[nucleotide]
+    end
+  end
+  return converted
 end
