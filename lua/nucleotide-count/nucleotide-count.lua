730248171a1af2482feaa201f84f20821c6f8823
Chip Castle <chip@chipcastle.com>
2021-09-06 13:41:05 -0500

    [Lua] Add solution for nucleotide-count

--- lua/nucleotide-count/nucleotide-count.lua
+++ lua/nucleotide-count/nucleotide-count.lua
@@ -1,3 +1,27 @@
 local DNA = {}
 
+local function parse (s)
+  local counts = { A = 0, T = 0, C = 0, G = 0 }
+  for i=1,#s do
+    local nucleotide = s:sub(i,i)
+    if not nucleotide:match("[ATCG]+") then error"Invalid Nucleotide" end
+    counts[nucleotide] = tonumber(counts[nucleotide]) + 1
+  end
+  return counts
+end
+
+function DNA:new (strand)
+  local o = {}
+  setmetatable(o, self)
+  self.__index = self
+  self.strand = strand
+  self.nucleotideCounts = parse(strand)
+  return o
+end
+
+function DNA:count (nucleotide)
+  if not nucleotide:match("[ATCG]+") then error"Invalid Nucleotide" end
+  return self.nucleotideCounts[nucleotide]
+end
+
 return DNA
