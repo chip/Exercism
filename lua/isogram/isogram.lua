e3d71e5c8fcd14094a443926aa66528c00b379ee
Chip Castle <chip@chipcastle.com>
2021-09-06 10:16:52 -0500

    [Lua] Add solution for isogram

--- lua/isogram/isogram.lua
+++ lua/isogram/isogram.lua
@@ -1,3 +1,18 @@
-return function(s)
+return function(str)
+  local s = str:lower()
+  local uniques = {}
 
+  for i=1,#s do
+    local char = s:sub(i,i)
+
+    if char:match("[a-z]+") then
+      if uniques[char] then
+        return false
+      else
+        uniques[char] = true
+      end
+    end
+  end
+
+  return true
 end
