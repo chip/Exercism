2a0d71abb51a79f3aec815c227a26f0faa7a3db2
Chip Castle <chip@chipcastle.com>
2021-09-06 17:24:04 -0500

    [Lua] Add solution for sum-of-multiples

--- lua/sum-of-multiples/sum-of-multiples.lua
+++ lua/sum-of-multiples/sum-of-multiples.lua
@@ -1,3 +1,31 @@
 return function(numbers)
+  local multiples = {}
 
+  local function reduce(list)
+    local sum = 0
+
+    for n,_ in pairs(list) do
+      sum = sum + n
+    end
+
+    return sum
+  end
+
+  local function to(limit)
+    for i=1,#numbers do
+      local num = numbers[i]
+      local current = num
+
+      while current < limit do
+        if not multiples[current] then
+          multiples[current] = true
+        end
+        current = current + num
+      end
+    end
+
+    return reduce(multiples)
+  end
+
+  return { to = to }
 end
