77143e2c778ba7c67255a33caaaa2336e947fb1b
Chip Castle <chip@chipcastle.com>
2021-09-06 12:46:54 -0500

    [Lua] Add solution for binary exercise

--- lua/binary/binary.lua
+++ lua/binary/binary.lua
@@ -1,5 +1,20 @@
 local function to_decimal(input)
+  if input:match("[a-zA-Z]") then
+    return 0
+  end
 
+  local sum = 0
+
+  for i=#input,1,-1 do
+    local bit = tonumber(input:sub(i,i))
+    if bit == 1 then
+      local power = #input - i
+      local result = 2 ^ power
+      sum = sum + result
+    end
+  end
+
+  return sum
 end
 
 return {
