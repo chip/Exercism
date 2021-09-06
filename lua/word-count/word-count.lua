3c8165d4ba9e510cf657d82714f733a4e7d2f19e
Chip Castle <chip@chipcastle.com>
2021-09-06 17:38:03 -0500

    [Lua] Add solution for word-count

--- lua/word-count/word-count.lua
+++ lua/word-count/word-count.lua
@@ -1,5 +1,22 @@
 local function word_count(s)
+  local words = {}
 
+  local function sanitize(current_token)
+    local word = current_token:lower()
+
+    word = word:gsub("^'", "")
+    word = word:gsub("'$", "")
+
+    return word
+  end
+
+  for token in s:gmatch("[%w']+") do
+    local word = sanitize(token)
+
+    words[word] = words[word] and (words[word] + 1) or 1
+  end
+
+  return words
 end
 
 return {
