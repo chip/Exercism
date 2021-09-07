184737ee78e57d7028d4a5c16e0f8e027a93d971
Chip Castle <chip@chipcastle.com>
2021-09-07 09:17:59 -0500

    [Lua] Add solution for pythagorean-triplet

--- lua/pythagorean-triplet/pythagorean-triplet.lua
+++ lua/pythagorean-triplet/pythagorean-triplet.lua
@@ -1,3 +1,21 @@
 return function(sum)
+  local triplets = {}
 
+  for a=2,sum do
+    local b_limit = sum - a
+    for b=(a+1),b_limit do
+      local ab = (a ^ 2) + (b ^ 2)
+      local sqrt_c = math.sqrt(ab)
+      if math.floor(sqrt_c) == sqrt_c then
+        for c=(b+1),sqrt_c do
+          local c2 = c ^ 2
+          if (ab == c2) and (a < b and b < c) and (a + b + c == sum) then
+            table.insert(triplets, {a, b ,c})
+          end
+        end
+      end
+    end
+  end
+
+  return triplets
 end
