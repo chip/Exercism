161564482ac2fbcf5981018b72caca52039e2461
Chip Castle <chip@chipcastle.com>
2021-09-08 11:24:40 -0500

    [Lua] Add 3rd solution for pythagorean-triplet

--- lua/pythagorean-triplet/pythagorean-triplet.lua
+++ lua/pythagorean-triplet/pythagorean-triplet.lua
@@ -1,22 +1,16 @@
 return function(sum)
+  local sqrt = math.sqrt
+  local floor = math.floor
   local triplets = {}
 
   local limit = sum / 2
-  local a = 3
-  while a < limit do
-    local b = a + 1
-    local b_limit = sum - a
-    while b < b_limit do
-      local c_squared = (a ^ 2) + (b ^ 2)
-      local c_root  = math.sqrt(c_squared)
-      local c = math.floor(c_root)
-      -- test that square root of c is an integer
-      if c == c_root and (a + b + c == sum) then
-        table.insert(triplets, {a, b ,c})
+  for a = 3, limit do
+    for b = (a + 1), (sum - a) do
+      local c = sqrt((a ^ 2) + (b ^ 2))
+      if c == floor(c) and ((a + b + c) == sum) then
+        triplets[#triplets + 1] = {a, b, c}
       end
-      b = b + 1
     end
-    a = a + 1
   end
 
   return triplets
