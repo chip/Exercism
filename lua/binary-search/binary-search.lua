445d704c05609086c4cd124e21a97f0d9919c250
Chip Castle <chip@chipcastle.com>
2021-09-08 19:28:47 -0500

    [Lua] Refactor again binary-search based on mentor feedback

--- lua/binary-search/binary-search.lua
+++ lua/binary-search/binary-search.lua
@@ -1,18 +1,10 @@
 return function(array, target)
-  local floor = math.floor
   local ceil = math.ceil
+  local index = #array == 1 and 1 or ceil(#array / 2)
   local found = false
-
-  local index = #array == 1 and 1 or floor(#array / 2)
-  print("target:"..target)
   local elements = #array
   while elements > 0 do
     local value = array[index]
-    print("index: ", index, " value: ", value, " elements: ", elements)
-    if value == nil then
-      elements = 0
-      break
-    end
 
     if target == value then
       found = true
@@ -20,25 +12,17 @@ return function(array, target)
       break
     end
 
+    elements = #array - index
+
     if target < value then
-      -- target = 2
-      -- local array = TracedArray{ 6, 67, 123, 345, 456, 457, 490, 2002, 54321, 54322 }
       if index == 1 then
         elements = 0
         break
       end
-      elements = #array - index
       index = ceil(index / 2)
     else
-      -- print("else", index)
-      elements = #array - index
-      index = index + ceil((#array - index) / 2)
+      index = index + ceil(elements / 2)
     end
-
   end
-  if found then
-    return index
-  else
-    return -1
-  end
+  return found and index or -1
 end
