a3db410cb01cf62a53df3d6f4ed8bcd3b5713f43
Chip Castle <chip@chipcastle.com>
2021-09-08 19:23:37 -0500

    [Lua] Refactor binary-search based on mentor feedback

--- lua/binary-search/binary-search.lua
+++ lua/binary-search/binary-search.lua
@@ -1,35 +1,44 @@
 return function(array, target)
-  local size = #array
-  local first = array[1]
-  local last = array[size]
+  local floor = math.floor
+  local ceil = math.ceil
+  local found = false
 
-  if size == 0 or (size == 1 and first ~= target) or target < first or target > last then
+  local index = #array == 1 and 1 or floor(#array / 2)
+  print("target:"..target)
+  local elements = #array
+  while elements > 0 do
+    local value = array[index]
+    print("index: ", index, " value: ", value, " elements: ", elements)
+    if value == nil then
+      elements = 0
+      break
+    end
+
+    if target == value then
+      found = true
+      elements = 0
+      break
+    end
+
+    if target < value then
+      -- target = 2
+      -- local array = TracedArray{ 6, 67, 123, 345, 456, 457, 490, 2002, 54321, 54322 }
+      if index == 1 then
+        elements = 0
+        break
+      end
+      elements = #array - index
+      index = ceil(index / 2)
+    else
+      -- print("else", index)
+      elements = #array - index
+      index = index + ceil((#array - index) / 2)
+    end
+
+  end
+  if found then
+    return index
+  else
     return -1
   end
-
-  if first == target then
-    return 1
-  end
-
-  if last == target then
-    return size
-  end
-
-  local index = math.ceil(size / 2)
-  local left = 1
-  local right = size
-
-  while left < right do
-    if array[index] == target then
-      break
-    elseif array[index] > target then
-      index = math.ceil(index / 2)
-      right = index
-    else
-      index = math.ceil(index + ((size - index) / 2))
-      left = index
-    end
-  end
-
-  return index
 end
