426e70f4154e3937a23f022fc2d9ceb07fd780da
Chip Castle <chip@chipcastle.com>
2021-10-18 13:17:04 -0500

    [Clojure] Refactor solution to robot-name exercise due to mentor feedback

--- clojure/robot-name/src/robot_name.clj
+++ clojure/robot-name/src/robot_name.clj
@@ -1,47 +1,25 @@
-(ns robot-name
-  (:require [clojure.string :as str]))
+(ns robot-name)
 
 (def memory (atom {}))
 (def robots (atom #{}))
 
-(defn random-letters []
-  (str/join "" (take 2 (repeatedly #(char (+ (rand 26) 65))))))
-
-(defn random-numbers []
-  (str/join "" (take 3 (repeatedly #(rand-int 10)))))
-
 (defn random-name []
-  (let [its-name (str (random-letters) (random-numbers))]
+  (let [letters (repeatedly 2 #(char (+ (rand 26) 65)))
+        numbers (repeatedly 3 #(rand-int 10))
+        its-name (apply str (concat letters numbers))]
     (if (contains? @robots its-name) (recur) its-name)))
 
-(defn random-key []
-  (keyword (gensym "robot-")))
-
-(defn get-name [robot-key]
-  (if-let [robot-name (get @memory robot-key)]
-    robot-name
-    nil))
-  
 (defn update-memory [robot-key robot-name]
   (swap! robots conj robot-name)
-  (swap! memory conj (hash-map robot-key robot-name)))
-
-(defn robot-exists? [robot-key]
-  (contains? @robots (get-name robot-key)))
+  (swap! memory assoc robot-key robot-name))
 
 (defn robot []
-  (let [robot-key (random-key) robot-name (random-name)]
+  (let [robot-key (keyword (gensym "robot-")) robot-name (random-name)]
     (update-memory robot-key robot-name)
     robot-key))
 
 (defn robot-name [robot-key]
-  (get-name robot-key))
+  (@memory robot-key))
 
 (defn reset-name [robot-key]
-  (let [robot-name (random-name)]
-    (if (robot-exists? robot-key)
-      (do
-        (update-memory robot-key robot-name)
-        robot-key
-      )
-      nil)))
+  (when (contains? @robots (@memory robot-key)) (update-memory robot-key (random-name))))
