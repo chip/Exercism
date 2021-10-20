a0ff626530d0b00f3c2c63e8bb5292dae5860747
Chip Castle <chip@chipcastle.com>
2021-10-20 16:24:56 -0500

    [Clojure] Add solution for interest-is-interesting exercise

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -1,16 +1,28 @@
-(ns interest-is-interesting)
+(ns interest-is-interesting
+  (:require [clojure.math.numeric-tower :as math]))
 
 (defn interest-rate
-  "TODO: add docstring"
+  "Calculate the interest rate based on the specified balance"
   [balance]
-  )
+  (let [rate (cond
+      (< balance 0M) -3.213
+      (< balance 1000M) 0.5
+      (< balance 5000M) 1.621
+      (>= balance 5000M) 2.475)]
+    (double rate)
+    ))
 
 (defn annual-balance-update
-  "TODO: add docstring"
+  "Calculate the annual balance update, taking into account the interest rate"
   [balance]
-  )
+  (let [interest-rate-converted (bigdec (/ (interest-rate balance) 100M))
+        interest-earned (bigdec (* interest-rate-converted (math/abs balance)))]
+    (with-precision 26 (+ interest-earned balance))))
 
 (defn amount-to-donate
-  "TODO: add docstring"
+  "Calculate how much money to donate to charities based on the balance and the
+  tax-free percentage that the government allows"
   [balance tax-free-percentage]
-  )
+  (if (pos? balance)
+    (int (* (* (/ tax-free-percentage 100M) balance) 2))
+    0))
