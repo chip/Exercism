62b3e1266fa8a36a245faf86d4b3572cfa6617c2
Chip Castle <chip@chipcastle.com>
2021-10-20 16:42:43 -0500

    [Clojure] Refactor solution to interest-is-interesting exercise

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -4,25 +4,28 @@
 (defn interest-rate
   "Calculate the interest rate based on the specified balance"
   [balance]
-  (let [rate (cond
-      (< balance 0M) -3.213
-      (< balance 1000M) 0.5
-      (< balance 5000M) 1.621
-      (>= balance 5000M) 2.475)]
-    (double rate)
-    ))
+  (double (cond
+            (< balance 0M) -3.213
+            (< balance 1000M) 0.5
+            (< balance 5000M) 1.621
+            (>= balance 5000M) 2.475)))
+
+(defn interest-multiplier [rate] (/ rate 100M))
+
+(defn interest-earned
+  "Calculate the interest earned based on the specified balance"
+  [balance]
+  (* (bigdec (interest-multiplier (interest-rate balance))) (math/abs balance)))
 
 (defn annual-balance-update
   "Calculate the annual balance update, taking into account the interest rate"
   [balance]
-  (let [interest-rate-converted (bigdec (/ (interest-rate balance) 100M))
-        interest-earned (bigdec (* interest-rate-converted (math/abs balance)))]
-    (with-precision 26 (+ interest-earned balance))))
+  (with-precision 26 (+ (interest-earned balance) balance)))
 
 (defn amount-to-donate
   "Calculate how much money to donate to charities based on the balance and the
   tax-free percentage that the government allows"
   [balance tax-free-percentage]
   (if (pos? balance)
-    (int (* (* (/ tax-free-percentage 100M) balance) 2))
+    (int (* (* (interest-multiplier tax-free-percentage) balance) 2))
     0))
