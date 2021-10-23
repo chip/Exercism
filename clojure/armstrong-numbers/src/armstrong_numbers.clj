f7f43b585ba2636eddc2b527071bd27d8e13c71e
Chip Castle <chip@chipcastle.com>
2021-10-23 15:22:43 -0500

    [Clojure] Refactor solution to armstrong-numbers exercise due to inability for exercism's build system to load libraries

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -1,15 +1,23 @@
-(ns armstrong-numbers
-  (:require [clojure.math.numeric-tower :as math]))
+(ns armstrong-numbers)
 
-(defn parse-digits
+(defn digits
   "Accept integer and parse into individual digits"
   [num]
-  (map #(- (int %1) 48) (seq (str num))))
+  (map #(- (int %) 48) (str num)))
+
+(defn raised
+  "Multiply base by itself exp number of times"
+  [base exp]
+  (reduce * (repeat exp base)))
+
+(defn products
+  [num]
+  (let [pow (count (str num))]
+    (map #(raised % pow) (digits num))))
 
 (defn armstrong?
   "An Armstrong number is a number that is the sum of its own digits each
   raised to the power of the number of digits. Return boolean to indicate
   whether argument passed is a valid Armstrong number."
   [num]
-  (let [pow (count (str num))]
-    (= num (reduce + (map #(math/expt %1 pow) (parse-digits num))))))
+  (= num (apply + (products num))))
