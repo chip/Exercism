a5b4538c272cf5e6b396a89b297a03768bbe33bf
Chip Castle <chip@chipcastle.com>
2021-10-24 18:34:39 -0500

    [Clojure] Add solution for accumulate exercise

--- clojure/accumulate/src/accumulate.clj
+++ clojure/accumulate/src/accumulate.clj
@@ -1,5 +1,48 @@
 (ns accumulate)
 
-(defn accumulate [] ;; <- arglist goes here
-      ;; your code goes here
+; (defn- to-s [xs] (apply str xs))
+
+(defn accumulate
+  [f & args]
+  (prn "f " f)
+  (prn "args " args)
+  (prn "type args " (type args))
+  (let [v (first args)]
+    (prn "v " v)
+    (prn "type v: " (type v))
+    (if (empty? v)
+      (do 
+        (prn "do ")
+        v
+        )
+      (do
+        (prn "else do ")
+        (loop [coll v accum []]
+          (prn "coll: " coll)
+          (prn "count " (count coll))
+          (prn "accum: " accum)
+
+          (if (= zero? (rest coll))
+            accum
+            (do
+              (prn "else ")
+              (let [r (rest coll) item (first coll)]
+                (prn "rest " r)
+                (prn "item " item)
+                (if (nil? item)
+                  accum
+                  (do
+                    (let [result (f item)]
+                      (prn "result " result)
+                      (recur r (conj accum result))
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
 )
