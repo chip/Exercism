a182e87a7932866732c2215005c5b87bfde0c537
Chip Castle <chip@chipcastle.com>
2021-10-26 11:33:42 -0500

    [Clojure] Add solution for acronym exercise

--- clojure/acronym/src/acronym.clj
+++ clojure/acronym/src/acronym.clj
@@ -1,5 +1,19 @@
-(ns acronym)
+(ns acronym
+  (:require [clojure.string :as str]))
 
-(defn acronym [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn words [s]
+  (str/split s #"\b"))
+
+(defn capitalize [s]
+  (let [len (count s) first-char (subs s 0 1) remaining-chars (subs s 1 len)]
+    (str (str/upper-case first-char) remaining-chars)))
+
+(defn acronym [s]
+  (if (empty? s)
+    s
+    (str/join
+      (map #(subs % 0 1)
+           (map capitalize
+                (filter #(re-find #"^[A-Za-z]" %)
+                        (words
+                          (str/replace s #"([A-Z]{1}[a-z]+)" "$1 "))))))))
