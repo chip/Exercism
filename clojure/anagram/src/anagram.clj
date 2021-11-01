15c25e974b3c94acb6a4c537b7560a93c1cf3138
Chip Castle <chip@chipcastle.com>
2021-10-31 20:07:48 -0500

    [Clojure] Add solution for anagram exercise

--- clojure/anagram/src/anagram.clj
+++ clojure/anagram/src/anagram.clj
@@ -1,5 +1,12 @@
-(ns anagram)
+(ns anagram
+  (:require [clojure.string :as str]))
 
-(defn anagrams-for [word prospect-list] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn match [s d]
+  (= (frequencies s) (frequencies d)))
+
+(defn anagram [s d]
+  (or (and (not= s d) (match s d))
+      (match (str/lower-case s) (str/lower-case d))))
+
+(defn anagrams-for [word prospect-list]
+  (filter #(anagram word %) prospect-list))
