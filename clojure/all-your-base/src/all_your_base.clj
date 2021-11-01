644dd45e5dd81029949cd834b792dfd0059eb86a
Chip Castle <chip@chipcastle.com>
2021-10-31 19:17:50 -0500

    [Clojure] Refactor solution to all-your-base exercise

--- clojure/all-your-base/src/all_your_base.clj
+++ clojure/all-your-base/src/all_your_base.clj
@@ -1,50 +1,38 @@
 (ns all-your-base)
 
-(defn list-to-int [nums]
-  (Integer/parseInt (apply str nums)))
-
-(defn fmt [memo remainder]
-  (concat memo (list remainder)))
+(defn append-memo [memo x base]
+  (concat memo (list (rem x base))))
 
 (defn to-base [n base]
-  (loop [x n memo '()]
-    (let [quotient (quot x base) remainder (rem x base) data (fmt memo remainder)]
+  (loop [x (Integer/parseInt (apply str n)) memo '()]
+    (let [quotient (quot x base)]
       (if (zero? quotient)
-        (reverse data)
-        (recur quotient (flatten (conj data)))))))
+        (reverse (append-memo memo x base))
+        (recur quotient (flatten (conj (append-memo memo x base))))))))
 
-(defn num-to-list [x]
+(defn number-list [x]
   (for [n (str x)]
     (- (byte n) 48)))
 
-(defn decimal-to-binary? [b1 b2]
-  (and (= 10 b1) (= 2 b2)))
+(defn pow [[i n] b1]
+  (int (* n (Math/pow b1 i))))
+
+(defn powers-of [b1 nums]
+  (number-list (reduce + (map #(pow % b1) (reverse (map-indexed vector (reverse nums)))))))
+
+(defn process-data [b1 nums b2]
+  (let [data (powers-of b1 nums)]
+    (cond
+      (= b2 10) data
+      (= b2 2) (to-base nums b2)
+      :else (to-base data b2))))
 
 (defn valid-base? [base nums]
   (not (some #(or (neg? %) (>= % base)) nums)))
 
 (defn valid? [b1 nums b2]
-  (and (pos? b1) (not= 1 b1) (pos? b2) (not= 1 b2) (valid-base? b1 nums)))
-
-(defn power-of-base [[i n] b1]
-  (int (* n (Math/pow b1 i))))
-
-(defn powers [b1 nums]
-  (num-to-list
-    (reduce +
-            (map #(power-of-base % b1)
-              (reverse (map-indexed vector (reverse nums)))))))
+  (and (seq nums) (pos? b2) (not= 1 b2) (valid-base? b1 nums)))
 
 (defn convert [b1 nums b2]
   (if (valid? b1 nums b2)
-    (if (empty? nums)
-      (list)
-      (if (decimal-to-binary? b1 b2)
-        (to-base (list-to-int nums) b2)
-        (let [p1 (powers b1 nums)]
-          (cond
-            (= b2 16) (to-base (list-to-int p1) b2)
-            (= b2 3) (to-base (list-to-int p1) b2)
-            (= b2 73) (to-base (list-to-int p1) b2)
-            :else p1)
-          )))))
+    (process-data b1 nums b2)))
