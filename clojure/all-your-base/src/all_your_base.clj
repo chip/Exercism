8d1d14fcf3592109205f3ffbced1b157c75639bb
Chip Castle <chip@chipcastle.com>
2021-11-09 11:47:58 -0600

    [Clojure] Refactor solution to all-your-base exercise based on mentor feedback

--- clojure/all-your-base/src/all_your_base.clj
+++ clojure/all-your-base/src/all_your_base.clj
@@ -1,38 +1,21 @@
 (ns all-your-base)
 
-(defn append-memo [memo x base]
-  (concat memo (list (rem x base))))
-
 (defn to-base [n base]
-  (loop [x (Integer/parseInt (apply str n)) memo '()]
+  (loop [x n memo '()]
     (let [quotient (quot x base)]
       (if (zero? quotient)
-        (reverse (append-memo memo x base))
-        (recur quotient (flatten (conj (append-memo memo x base))))))))
-
-(defn number-list [x]
-  (for [n (str x)]
-    (- (byte n) 48)))
-
-(defn pow [[i n] b1]
-  (int (* n (Math/pow b1 i))))
+        (conj memo (rem x base))
+        (recur quotient (flatten (conj memo (rem x base))))))))
 
 (defn powers-of [b1 nums]
-  (number-list (reduce + (map #(pow % b1) (reverse (map-indexed vector (reverse nums)))))))
+  (reduce + (map #(* %1 %2) (iterate #(* % b1) 1) (reverse nums))))
 
 (defn process-data [b1 nums b2]
-  (let [data (powers-of b1 nums)]
-    (cond
-      (= b2 10) data
-      (= b2 2) (to-base nums b2)
-      :else (to-base data b2))))
-
-(defn valid-base? [base nums]
-  (not (some #(or (neg? %) (>= % base)) nums)))
+  (to-base (powers-of b1 nums) b2))
 
 (defn valid? [b1 nums b2]
-  (and (seq nums) (pos? b2) (not= 1 b2) (valid-base? b1 nums)))
+  (and (seq nums) (> b2 1) (> b1 1) (every? #(< -1 % b1) nums)))
 
 (defn convert [b1 nums b2]
-  (if (valid? b1 nums b2)
+  (when (valid? b1 nums b2)
     (process-data b1 nums b2)))
