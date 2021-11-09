83219ca07c9cde1ab3a4b8dd029d984ca92485da
Chip Castle <chip@chipcastle.com>
2021-11-09 12:30:32 -0600

    [Clojure] Refactor solution to pangram exercise

--- clojure/pangram/src/pangram.clj
+++ clojure/pangram/src/pangram.clj
@@ -2,11 +2,11 @@
   (:require [clojure.set :as set])
   (:require [clojure.string :as str]))
 
-(defn alpha []
-  (set '(\a \b \c \d \e \f \g \h \i \j \k \l \m \n \o \p \q \r \s \t \u \v \w \x \y \z)))
+(defn alphabet []
+  (set (seq "abcdefghijklmnopqrstuvwxyz")))
 
 (defn letters [s]
   (set (keys (frequencies (str/lower-case s)))))
 
 (defn pangram? [s]
-  (= 26 (count (set/intersection (alpha) (letters s)))))
+  (= 26 (count (set/intersection (alphabet) (letters s)))))
