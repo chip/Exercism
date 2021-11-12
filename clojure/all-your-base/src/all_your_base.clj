6a554acedd7ceeb6c823309fcde11f7df51e5a1f
Chip Castle <chip@chipcastle.com>
2021-11-12 10:59:22 -0600

    [Clojure] Refactor solution to all-your-base exercise based on mentor's feedback

--- clojure/all-your-base/src/all_your_base.clj
+++ clojure/all-your-base/src/all_your_base.clj
@@ -1,21 +1,23 @@
 (ns all-your-base)
 
-(defn to-base [n base]
-  (loop [x n memo '()]
-    (let [quotient (quot x base)]
-      (if (zero? quotient)
-        (conj memo (rem x base))
-        (recur quotient (flatten (conj memo (rem x base))))))))
+(defn to-base [n b]
+  (loop [q (quot n b), digits (list (rem n b))]
+    (if (zero? q)
+      digits
+      (recur (quot q b) (conj digits (rem q b))))))
 
-(defn powers-of [b1 nums]
-  (reduce + (map #(* %1 %2) (iterate #(* % b1) 1) (reverse nums))))
-
-(defn process-data [b1 nums b2]
-  (to-base (powers-of b1 nums) b2))
+(defn to-decimal [b1 nums]
+  (->> (reverse nums)
+       (map * (iterate #(* % b1) 1))
+       (reduce +)))
 
 (defn valid? [b1 nums b2]
-  (and (seq nums) (> b2 1) (> b1 1) (every? #(< -1 % b1) nums)))
+  (->> nums
+    (every? #(< -1 % b1))
+    (and (seq nums) (> b2 1) (> b1 1))))
 
 (defn convert [b1 nums b2]
   (when (valid? b1 nums b2)
-    (process-data b1 nums b2)))
+    (->
+      (to-decimal b1 nums)
+      (to-base b2))))
