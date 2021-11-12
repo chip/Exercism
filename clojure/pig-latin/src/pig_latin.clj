2d477e52a629a84c0b0ddad7b62fa7f1bbe56a4e
Chip Castle <chip@chipcastle.com>
2021-11-11 18:47:09 -0600

    [Clojure] Refactor solution to pig-latin exercise (3rd)

--- clojure/pig-latin/src/pig_latin.clj
+++ clojure/pig-latin/src/pig_latin.clj
@@ -3,50 +3,23 @@
 
 (def vowels #{\a \e \i \o \u})
 
-(def special-sounds '("squ" "qu" "thr" "th" "sch" "ch"))
-
-(defn special-consonant-sound? [s]
-  (some #(str/starts-with? s %) special-sounds))
-
-(defn starts-with-consonant? [s]
-  (not-any? #(contains? vowels %) (take 1 s)))
-
-(defn special-start [s]
-  (first (filter #(str/starts-with? s %) special-sounds)))
-
-(defn matcher-var [s]
-  (if (special-consonant-sound? s)
-    (str "{" (count (special-start s)) "}")
-    (str "*" s)))
-
-(defn matcher-str [s]
-  (str "^(." (matcher-var s) ")(.+)$"))
-
-(defn parse [s]
-  (let [match (re-find (re-pattern (matcher-str s)) s)]
-    (str (last match) (second match) "ay")))
-
 (defn starting-consonants [s]
   (take-while #(not (contains? vowels %)) s))
 
-(defn length [s]
-  (let [special (special-start s)]
-    (if special
-      (count special)
-      (count (starting-consonants s)))))
+(defn special-sounds-beginning [s]
+  (first (filter #(str/starts-with? s %) '("squ" "qu" "thr" "th" "sch" "ch"))))
 
-(defn join-str [s]
-  (let [n (length s)]
-    (str (subs s n (count s)) (subs s 0 n) "ay")))
+(defn length [s]
+  (let [beginning (special-sounds-beginning s)]
+    (if beginning (count beginning) (count (starting-consonants s)))))
 
 (defn vowel-sound? [s]
-  (or (not (starts-with-consonant? s)) (str/starts-with? s "yt") (str/starts-with? s "xr")))
+  (or (not (starting-consonants s)) (some #(str/starts-with? s %) '("xr" "yt"))))
 
 (defn process-word [s]
   (cond
     (vowel-sound? s) (str s "ay")
-    (or (special-consonant-sound? s) (starts-with-consonant? s)) (join-str s)
-    :else (parse s)))
+    :else (let [n (length s)] (str (subs s n (count s)) (subs s 0 n) "ay"))))
 
 (defn words [s]
   (str/split s #"\s"))
