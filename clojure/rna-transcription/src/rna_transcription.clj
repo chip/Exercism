ef502b5897c82e5d457518b5a6ba20f0427c2c22
Chip Castle <chip@chipcastle.com>
2021-11-29 11:45:55 -0600

    [Clojure] Add solution to rna-transcription exercise

--- clojure/rna-transcription/src/rna_transcription.clj
+++ clojure/rna-transcription/src/rna_transcription.clj
@@ -1,5 +1,12 @@
-(ns rna-transcription)
+(ns rna-transcription
+  (:require [clojure.string :as str]))
 
-(defn to-rna [dna] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def dna-to-rna {:G "C", :C "G", :T "A", :A "U"})
+
+(defn translate [dna]
+  (if-let [t (get dna-to-rna (keyword dna))]
+    t
+    (throw (AssertionError. "Invalid DNA strand"))))
+
+(defn to-rna [dna]
+  (str/join (map #(translate (first %)) (re-seq #"(.)" dna))))
