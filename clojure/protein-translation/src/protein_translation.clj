8ed97624ceffab6f159cf8422a67eec58520de9f
Chip Castle <chip@chipcastle.com>
2021-11-29 11:13:33 -0600

    [Clojure] Refactor solution to protein-translation exercise

--- clojure/protein-translation/src/protein_translation.clj
+++ clojure/protein-translation/src/protein_translation.clj
@@ -16,10 +16,17 @@
              :UGC "Cysteine"
              :UGG "Tryptophan"})
 
+(defn codon-seq [s]
+  (re-seq #"(.){3}" s))
+
 (defn translate-codon [s]
   (let [k (keyword s)]
-    (if (stop k) "STOP" (codons k))))
+    (if (stop k)
+      "STOP"
+      (codons k))))
+
+(defn valid [s]
+  (not= "STOP" s))
 
 (defn translate-rna [s]
-  (into [] (take-while #(not (= "STOP" %))
-                       (map #(translate-codon (first %)) (re-seq #"(.){3}" s)))))
+  (into [] (take-while valid (map #(translate-codon (first %1)) (codon-seq s)))))
