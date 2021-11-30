5f42d2c7e05a6d5716c9e00b26e4ad817e674c9a
Chip Castle <chip@chipcastle.com>
2021-11-30 17:57:45 -0600

    [Clojure] Add solution to raindrops exercise

--- clojure/raindrops/src/raindrops.clj
+++ clojure/raindrops/src/raindrops.clj
@@ -1,5 +1,11 @@
 (ns raindrops)
 
-(defn convert [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn sound [n [d s]]
+  (if (zero? (mod n d)) s ""))
+
+(defn raindrops [n]
+  (map #(sound n %) {3 "Pling" 5 "Plang" 7 "Plong"}))
+  
+(defn convert [n]
+  (let [s (apply str (raindrops n))]
+    (if (empty? s) (str n) s)))
