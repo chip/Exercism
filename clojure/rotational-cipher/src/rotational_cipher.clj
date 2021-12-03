b1d00cedbe1f0ea5cb1c37c9655a03b7f8939e4c
Chip Castle <chip@chipcastle.com>
2021-12-03 12:44:25 -0600

    [Clojure] Refactor solution to rotational-cipher exercise

--- clojure/rotational-cipher/src/rotational_cipher.clj
+++ clojure/rotational-cipher/src/rotational_cipher.clj
@@ -1,70 +1,32 @@
-(ns rotational-cipher
-  (:require [clojure.string :as str]))
+(ns rotational-cipher)
 
-(defn generate-letters [n] (into [] (map #(char (+ % n)) (range 26))))
+(def ascii-offset-A 65)
+(def ascii-offset-a 97)
+(def alphabet-count 26)
 
-; upper-case letters
-(defn upper-case-letters [] (generate-letters 65))
+(defn generate-letters [n] (into [] (map #(char (+ % n)) (range alphabet-count))))
 
-; lower-case letters
-(defn lower-case-letters [] (generate-letters 97))
+(defn letters [c]
+  (if (re-find #"[A-Z]" (str c))
+    (generate-letters ascii-offset-A) ; upper-case letters 
+    (generate-letters ascii-offset-a))) ; lower-case letterse
 
-; (prn "lower-case-letters:" (get (lower-case-letters) 4))
-
-(defn operator [n]
-  (if (pos? n) + -))
-
-(defn lower-case? [c]
-  (= (str c) (str/lower-case c)))
-
-(defn upper-case? [c]
-  (= (str c) (str/upper-case c)))
-; create letter mapping (see upper-case-letters, lower-case-letters)
-; find letter index
-; find offset (rem n 26)
-; find replacement letter index from offset
-; return letter using offset index
 (defn letter-index [c]
-  (cond
-    (lower-case? c) (first (keep-indexed #(when (= c %2) %1) (lower-case-letters)))
-    (upper-case? c) (first (keep-indexed #(when (= c %2) %1) (upper-case-letters)))
-    ))
+  (first (keep-indexed #(when (= c %2) %1) (letters c))))
+
+(defn ascii-offset [c]
+  (if (re-find #"[A-Z]" (str c))
+    ascii-offset-A
+    ascii-offset-a))
 
 (defn process [c n]
-  (prn "process c n" c n)
-  (let [i (letter-index c)]
-    (cond 
-      (nil? i) (do
-                 (prn "skipping" c)
-                 c
-                 )
-      :else (let [o (mod (+ n i) 26)]
-              (prn "n i o " n i o)
-              (if (lower-case? c)
-                (char (+ o 97))
-                (char (+ o 65)))
-                ; (prn "remainder" remainder)
-                ; (prn "nth" (nth (lower-case-letters) i))
-                ; (let [r (char ((operator remainder) remainder i))]
-                ; r
-                ))))
-
-(lower-case-letters)
-; find remainder of n 26
-; if remainder is negative subtract from letter index - otherwise, add
-;
+  (let [i (letter-index c) o (mod (+ n i) 26)]
+    (char (+ o (ascii-offset c)))))
 
 (defn map-char [c n]
-  ; (prn "map-char c" c)
-  ; (prn "char + int c 65 n" (char (+ (int c) n)))
-  (cond 
-    (or (lower-case? c) (upper-case? c)) (process c n)
-    :else c
-    ))
+  (cond
+    (re-find #"[A-Za-z]" (str c)) (process c n)
+    :else c))
 
 (defn rotate [s n]
-  ; (prn "n" n)
   (apply str (map #(map-char %1 n) s)))
-
-; (rotate "fob" 1)
-; (rotational-cipher/rotate "Testing 1 2 3 testing" 4); "Xiwxmrk 1 2 3 xiwxmrk")))
