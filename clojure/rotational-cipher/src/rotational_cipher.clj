c233ca27be59ed33d9db4ee9e037b589ad2fffb0
Chip Castle <chip@chipcastle.com>
2021-12-03 13:37:26 -0600

    [Clojure] Refactor solution to rotational-cipher exercise (3rd)

--- clojure/rotational-cipher/src/rotational_cipher.clj
+++ clojure/rotational-cipher/src/rotational_cipher.clj
@@ -1,21 +1,11 @@
 (ns rotational-cipher
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str :only [index-of]]))
 
 (def uppercase "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 (def lowercase "abcdefghijklmnopqrstuvwxyz")
 (def ascii-offset-A 65)
 (def ascii-offset-a 97)
 (def alphabet-count 26)
-
-(defn upper-case?
-  "Determine if letter is uppercase A-Z"
-  [c]
-  (re-find #"[A-Z]" (str c)))
-
-(defn lower-case?
-  "Determine if letter is lowercase a-z"
-  [c]
-  (re-find #"[a-z]" (str c)))
 
 (defn convert-char
   "Find index i of letters in lowercase/uppercase string.
@@ -30,8 +20,8 @@
   "Process upper and lower case letters; skip others"
   [c n]
   (cond
-    (upper-case? c) (convert-char c n uppercase ascii-offset-A) 
-    (lower-case? c) (convert-char c n lowercase ascii-offset-a) 
+    (re-find #"[A-Z]" (str c)) (convert-char c n uppercase ascii-offset-A) 
+    (re-find #"[a-z]" (str c)) (convert-char c n lowercase ascii-offset-a) 
     :else c))
 
 (defn rotate
