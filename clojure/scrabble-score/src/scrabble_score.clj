0c1c38d2fae0a20ae8dd7a535688f3e6c0c99c29
Chip Castle <chip@chipcastle.com>
2021-12-03 17:14:57 -0600

    [Clojure] Refactor solution to scrabble-score exercise

--- clojure/scrabble-score/src/scrabble_score.clj
+++ clojure/scrabble-score/src/scrabble_score.clj
@@ -1,36 +1,16 @@
 (ns scrabble-score
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str ]))
 
 (defn score-letter [s]
   (let [c (str/lower-case s)]
     (cond
-      (= c "a") 1
-      (= c "b") 3
-      (= c "c") 3
-      (= c "d") 2
-      (= c "e") 1
-      (= c "f") 4
-      (= c "g") 2
-      (= c "h") 4
-      (= c "i") 1
-      (= c "j") 8
-      (= c "k") 5
-      (= c "l") 1
-      (= c "m") 3
-      (= c "n") 1
-      (= c "o") 1
-      (= c "p") 3
-      (= c "q") 10
-      (= c "r") 1
-      (= c "s") 1
-      (= c "t") 1
-      (= c "u") 1
-      (= c "v") 4
-      (= c "w") 4
-      (= c "x") 8
-      (= c "y") 4
-      (= c "z") 10
-      :else 0)))
+      (str/index-of "dg" c) 2
+      (str/index-of "bcmp" c) 3
+      (str/index-of "fhvwy" c) 4
+      (str/index-of "k" c) 5
+      (str/index-of "jx" c) 8
+      (str/index-of "qz" c) 10 
+      :else 1)))
 
 (defn score-word [w]
   (reduce + (map score-letter w)))
