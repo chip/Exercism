929a80d7beeecff1be1726c377b49c3788592f40
Chip Castle <chip@chipcastle.com>
2021-12-05 12:08:31 -0600

    [Clojure] Add solution to secret-handshake exercise

--- clojure/secret-handshake/src/secret_handshake.clj
+++ clojure/secret-handshake/src/secret_handshake.clj
@@ -1,5 +1,18 @@
 (ns secret-handshake)
 
-(defn commands [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn too-big? [n]
+  (>= n 32))
+
+(defn commands [n]
+  (if (too-big? n)
+    (vector)
+    (let [sorted? (>= n 16)]
+      (loop [x (if sorted? (- n 16) n) events []]
+        (cond 
+          (zero? x) (if sorted? events (reverse events))
+          :else
+            (cond
+              (>= x 8) (recur (- x 8) (conj events "jump"))
+              (>= x 4) (recur (- x 4) (conj events "close your eyes"))
+              (>= x 2) (recur (- x 2) (conj events "double blink"))
+              (>= x 1) (recur (- x 1) (conj events "wink"))))))))
