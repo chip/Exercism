8aed4ba0e775daa4354ffeb654c78a27761b1b04
Chip Castle <chip@chipcastle.com>
2021-12-06 10:37:15 -0600

    [Clojure] Add solution to space-age exercise

--- clojure/space-age/src/space_age.clj
+++ clojure/space-age/src/space_age.clj
@@ -1,33 +1,28 @@
 (ns space-age)
 
-(defn on-mercury [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(def earth-orbit-in-seconds (float 31557600.0))
 
-(defn on-venus [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-earth [n]
+  (/ n earth-orbit-in-seconds))
 
-(defn on-earth [] ;; <- arglist goes here
-      ;; your code goes here
-)
+;; All floats below represent orbital period in earth years for a given planet
+(defn on-mercury [n]
+  (/ (on-earth n) (float 0.2408467)))
 
-(defn on-mars [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-venus [n]
+  (/ (on-earth n) (float 0.61519726)))
 
-(defn on-jupiter [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-mars [n]
+  (/ (on-earth n) (float 1.8808158)))
 
-(defn on-saturn [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-jupiter [n]
+  (/ (on-earth n) (float 11.862615)))
 
-(defn on-neptune [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-saturn [n]
+  (/ (on-earth n) (float 29.447498)))
 
-(defn on-uranus [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn on-uranus [n]
+  (/ (on-earth n) (float 84.016846)))
+
+(defn on-neptune [n]
+  (/ (on-earth n) (float 164.79132)))
