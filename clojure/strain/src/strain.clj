511f27887048395287b7f25e45f74ec9a5720113
Chip Castle <chip@chipcastle.com>
2021-12-06 16:57:21 -0600

    [Clojure] Add solution for strain exercise

--- clojure/strain/src/strain.clj
+++ clojure/strain/src/strain.clj
@@ -1,9 +1,16 @@
 (ns strain)
 
-(defn retain [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn retain [pred coll]
+  (loop [retained [] remaining coll]
+    (if (empty? remaining)
+      retained
+      (if (pred (first remaining))
+        (recur (conj retained (first remaining)) (rest remaining))
+        (recur retained (rest remaining))))))
 
-(defn discard [] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn discard [pred coll]
+  (if (empty? coll)
+    coll
+    (if (pred (first coll))
+      (discard pred (rest coll))
+      (cons (first coll) (discard pred (rest coll))))))
