7950d396b6a7ceb461b53d587109ed72e2ca2d57
Chip Castle <chip@chipcastle.com>
2021-12-06 17:27:53 -0600

    [Clojure] Refactor solution to strain exercise based on community provided solutions

--- clojure/strain/src/strain.clj
+++ clojure/strain/src/strain.clj
@@ -1,18 +1,10 @@
 (ns strain)
 
 (defn retain [pred coll]
-  (loop [retained [] remaining coll]
-    (let [item (first remaining) remaining-items (rest remaining)]
-      (if (empty? remaining)
-        retained
-        (if (pred item)
-          (recur (conj retained item) remaining-items)
-          (recur retained remaining-items))))))
+  (reduce (fn [retained item]
+            (if (pred item) (conj retained item) retained))
+            []
+            coll))
 
 (defn discard [pred coll]
-  (if (empty? coll)
-    coll
-    (let [item (first coll) remaining-items (rest coll)]
-      (if (pred item)
-        (discard pred remaining-items)
-        (cons item (discard pred remaining-items))))))
+  (retain (complement pred) coll))
