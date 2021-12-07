ff4d9026fd9c55b596d1c7b582828ba34eb3b374
Chip Castle <chip@chipcastle.com>
2021-12-07 08:52:17 -0600

    [Clojure] Add solution for sublist exercise

--- clojure/sublist/src/sublist.clj
+++ clojure/sublist/src/sublist.clj
@@ -1,5 +1,10 @@
 (ns sublist)
 
-(defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn classify [list1 list2]
+  (let [s (apply str list1) c (count s) s2 (apply str list2) c2 (count s2)]
+    (cond
+      (= list1 list2) :equal
+      (and (re-find (re-pattern s) s2) (> c2 c)) :sublist
+      (and (re-find (re-pattern s2) s) (> c c2)) :superlist
+      :else :unequal
+      )))
