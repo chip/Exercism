3f8151ffc99b3fa60652f2210b712071c9b1d1d7
Chip Castle <chip@chipcastle.com>
2021-12-09 11:08:16 -0600

    [Clojure] Add solution for sum-of-multiples exercise

--- clojure/sum-of-multiples/src/sum_of_multiples.clj
+++ clojure/sum-of-multiples/src/sum_of_multiples.clj
@@ -1,5 +1,10 @@
 (ns sum-of-multiples)
 
-(defn sum-of-multiples [] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn multiples-of-n-to-upper-limit [n limit]
+  (take-while #(> limit %) (iterate (partial + n) n)))
+
+(defn sum-of-multiples [list1 limit]
+  (->> list1
+       (mapcat #(multiples-of-n-to-upper-limit % limit))
+       (distinct)
+       (reduce +)))
