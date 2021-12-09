f51eeced8d0417ee2dd527b954a0a41bf70bc964
Chip Castle <chip@chipcastle.com>
2021-12-09 11:13:48 -0600

    [Clojure] Refactor solution to sum-of-multiples exercise

--- clojure/sum-of-multiples/src/sum_of_multiples.clj
+++ clojure/sum-of-multiples/src/sum_of_multiples.clj
@@ -1,9 +1,13 @@
 (ns sum-of-multiples)
 
-(defn multiples-of-n-to-upper-limit [n limit]
+(defn multiples-of-n-to-upper-limit
+  "Calculate multiples of n up to but not including limit"
+  [n limit]
   (take-while #(> limit %) (iterate (partial + n) n)))
 
-(defn sum-of-multiples [list1 limit]
+(defn sum-of-multiples
+  "Calculate sum of unique multiples"
+  [list1 limit]
   (->> list1
        (mapcat #(multiples-of-n-to-upper-limit % limit))
        (distinct)
