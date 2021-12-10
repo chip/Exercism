881531a1bebb1a1af0f3383fcb1d2d1627541a61
Chip Castle <chip@chipcastle.com>
2021-12-10 13:01:29 -0600

    [Clojure] Add solution for word-count exercise

--- clojure/word-count/src/word_count.clj
+++ clojure/word-count/src/word_count.clj
@@ -1,5 +1,9 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string :as str]))
 
-(defn word-count [s] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn word-count
+  "Given a phrase, count the occurrences of each word in that phrase"
+  [s]
+  (->> (str/lower-case s)
+       (re-seq #"\w+")
+       (frequencies)))
