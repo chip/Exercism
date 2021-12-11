29734bd0f084818a016121970e849189da0b5b60
Chip Castle <chip@chipcastle.com>
2021-12-10 18:02:07 -0600

    [Clojure] Refactor solution to run-length-encoding exercise based on mentor feedback

--- clojure/run-length-encoding/src/run_length_encoding.clj
+++ clojure/run-length-encoding/src/run_length_encoding.clj
@@ -1,63 +1,28 @@
 (ns run-length-encoding)
 
-(defn matcher
-  "return appropriate regex match per given action"
-  [text action]
-  (if (= action :encode)
-    (re-matcher #"([A-Za-z ])\1*" text)
-    (re-matcher #"([0-9]+)?([A-Za-z ]{1})" text)))
-
-(defn letter-count
-  "return letter count if more than 1 is found"
-  [m]
-  (let [c (count m)]
-    (when (> c 1)
-      c)))
-
-(defn encode
-  "encode a regex match"
-  [match]
-  (let [[full-match letter] match]
-    (str (letter-count full-match) letter)))
-
-(defn match-count-to-int
-  "convert regex match count to an integer: nil or single space to 1; \"3\" to 3"
-  [s]
-  (cond 
-    (or (nil? s) (= " " s)) 1
-    :else (Integer/parseInt s)))
-
-(defn decode
-  "decode a regex match"
-  [match]
-  (let [[_ c letter] match, n (match-count-to-int c)]
-    (apply str (repeat n letter))))
-
-(defn build-str
-  "build string based on regex match; invoke fn based upon action arg"
-  [action match]
-  (if (= action :encode)
-    (encode match)
-    (decode match)))
-
-(defn parse
-  "parse text according to action"
-  [text action]
-  (let [m (matcher text action)]
-    (loop [acc ""]
-      (let [match (re-find m)]
-        (if match
-          (recur (str acc (build-str action match)))
-          acc)))))
+(defn char-count
+  "return character count only when greater than 1"
+  [matches]
+  (when (> (count matches) 1)
+    (count matches)))
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
   [s]
-  (if (empty? s)
-    s
-    (parse s :encode)))
+  (->> s
+    (partition-by identity)
+    (map #(str (char-count %) (str (first %))))
+    (reduce str "")))
+
+(defn decode
+  "accepts regex matcher vector to determine how to repeat letters"
+  [[_ n letter]]
+  (if-not (nil? n)
+    (apply str (repeat (int (bigdec n)) letter))
+    (str letter)))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [s]
-  (parse s :decode))
+  (let [matcher (re-seq #"([0-9]+)?([A-Za-z ]{1})" s)]
+    (reduce str "" (map decode matcher))))
