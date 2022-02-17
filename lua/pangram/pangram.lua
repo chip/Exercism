d7b80a35e0da93bbfadf836430d2c6e72b2c7b46
Chip Castle <chip@chipcastle.com>
2022-02-17 12:32:02 -0600

    [Lua] Refactor solution for pangram exercise

--- lua/pangram/pangram.lua
+++ lua/pangram/pangram.lua
@@ -3,21 +3,54 @@ return function(s)
   if s == '' or #s < alphabet_size then return false end
 
   s = string.lower(s)
+  local table_size = 0
   local letter_counts = {}
   for i = 1, #s, 1 do
     local letter = string.sub(s, i, i)
     if string.match(letter, "[%a]+") then
       local count = (letter_counts[letter] or 0) + 1
       letter_counts[letter] = count
+      table_size = table_size + 1
     end
   end
 
-  local table_count = 0
-  for letter, _ in pairs(letter_counts) do
-    table_count = table_count + 1
-    if letter_counts[letter] >= alphabet_size then return false end
+  local alphabet = {
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  }
+  local is_pangram = true
+  for _, letter in ipairs(alphabet) do
+    if letter_counts[letter] == nil then is_pangram = false end
   end
-  if table_count < alphabet_size then return false end
 
-  return true
+  for letter, _ in pairs(letter_counts) do
+    if letter_counts[letter] >= alphabet_size then is_pangram = false end
+  end
+  if table_size < alphabet_size then is_pangram = false end
+
+  return is_pangram
 end
