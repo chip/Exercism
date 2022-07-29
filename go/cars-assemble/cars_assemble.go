40eacfe75be9cfc58fdc205d90e3e6efe507db1c
Chip Castle <chip@chipcastle.com>
2022-07-29 18:01:21 -0500

    [Go] Add solution for cars-assemble exercise

--- go/cars-assemble/cars_assemble.go
+++ go/cars-assemble/cars_assemble.go
@@ -1,18 +1,32 @@
 package cars
 
+import "math"
+
 // CalculateWorkingCarsPerHour calculates how many working cars are
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
-	panic("CalculateWorkingCarsPerHour not implemented")
+	return float64(productionRate) * successRate / 100
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
-	panic("CalculateWorkingCarsPerMinute not implemented")
+	CarsPerHour := CalculateWorkingCarsPerHour(productionRate, successRate)
+	CarsPerMinute := CarsPerHour / float64(60)
+	return int(math.Floor(CarsPerMinute))
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	costOfOneCar := 10000
+	groupsOfTen := carsCount / 10
+	if groupsOfTen == 0 {
+		return uint(carsCount) * uint(costOfOneCar)
+	}
+	costOfTenCars := 95000
+	groupCost := groupsOfTen * costOfTenCars
+	remainder := carsCount - (groupsOfTen * 10)
+	remainderCost := remainder * costOfOneCar
+
+	return uint(groupCost) + uint(remainderCost)
 }
