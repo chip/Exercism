2d0e7912b9579c2a05fd2d50a8e5286101fd548f
Chip Castle <chip@chipcastle.com>
2022-07-29 16:57:46 -0500

    [Go] Add solution for weather-forecast

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather is used for weather forecasting in the country of Goblinocus.
 package weather
 
+// CurrentCondition describes the current weather.
 var CurrentCondition string
+
+// CurrentLocation describes the current city in Goblinocus.
 var CurrentLocation string
 
+// Forecast returns the current weather conditions given the city and condition provided.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
