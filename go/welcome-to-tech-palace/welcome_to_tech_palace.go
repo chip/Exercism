d810bb7b4e16bee7434104c4bcfa403a8fc9f29d
Chip Castle <chip@chipcastle.com>
2022-07-29 20:54:01 -0500

    [Go] Add solution for welcome-to-tech-palace exercise

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,27 @@
 package techpalace
 
+import (
+	"fmt"
+	"strings"
+)
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	var b strings.Builder
+	fmt.Fprintf(&b, "Welcome to the Tech Palace, %s", strings.ToUpper(customer))
+	return b.String()
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	stars := strings.Repeat("*", numStarsPerLine)
+	s := []string{stars, welcomeMsg, stars}
+	return strings.Join(s, "\n")
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	s := strings.ReplaceAll(oldMsg, "*", "")
+	s = strings.ReplaceAll(s, "\n", "")
+	return strings.TrimSpace(s)
 }
