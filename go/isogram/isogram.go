9cb7f2214de0bdcd81257933dd003450e6004af2
Chip Castle <chip@chipcastle.com>
2022-08-01 10:07:20 -0500

    [Go] Add solution to isogram exercise

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,5 +1,27 @@
+// Package isogram can determine if a word or phrase is without a repeating letter
+// package isogram
 package isogram
 
+import "strings"
+
+// Determine if a given word is a "non-pattern" word (i.e., contains no
+// repeating letters, excluding spaces and hyphens)
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	if word == "" {
+		return true
+	}
+	isogram := true
+	letterSet := map[rune]struct{}{}
+	for _, letter := range strings.ToLower(word) {
+		if letter == ' ' || letter == '-' {
+			continue
+		}
+		_, found := letterSet[letter]
+		if found {
+			isogram = false
+			break
+		}
+		letterSet[letter] = struct{}{}
+	}
+	return isogram
 }
