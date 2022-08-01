53385f96b9545d8515b33a7771e59e809b790bfa
Chip Castle <chip@chipcastle.com>
2022-08-01 14:29:38 -0500

    [Go] Add solution for difference-of-squares exercise

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,37 @@
+// Package diffsquares can be used to calculate the square of a sum of numbers or sum of squares
 package diffsquares
 
+// Given n and some function fn, return the sum of each fn calculation.
+// Given n and a nil function, return the sum of each number up to n.
+func reduce(n int, fn func(x int) int) int {
+	sum := 0
+	for i := 1; i <= n; i++ {
+		if fn != nil {
+			sum += fn(i)
+		} else {
+			sum += i
+		}
+	}
+	return sum
+}
+
+// Return the square of a given number x
+func square(x int) int {
+	return x * x
+}
+
+// Return the square of a sum of numbers
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	sum := reduce(n, nil)
+	return sum * sum
 }
 
+// Return the sum for all n squares
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	return reduce(n, square)
 }
 
+// Return the difference of the two calculations above
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
