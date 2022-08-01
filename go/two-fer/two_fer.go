e9b5caaca89bea8f7aab01e0420f49494162af5c
Chip Castle <chip@chipcastle.com>
2022-08-01 07:06:08 -0500

    [Go] Add solution for two-fer exercise

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,15 +1,14 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer returns a greeting for a given name argument. If no name is provided, name is assigned to "you".
 package twofer
 
-// ShareWith should have a comment documenting it.
+import "fmt"
+
+// ShareWith accepts a name string argument and returns a string greeting
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	defaultName := "you"
+	greeting := "One for %s, one for me."
+	if name == "" {
+		name = defaultName
+	}
+	return fmt.Sprintf(greeting, name)
 }
