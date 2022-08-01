13049c9b0d6a5a521e34d4c6b30847d5576f4aeb
Chip Castle <chip@chipcastle.com>
2022-08-01 07:57:36 -0500

    [Go] Add solution to scrabble-score exercise

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,28 @@
+// Package scrabble provides a function for computing the Scrabble score of a given word
 package scrabble
 
+import "strings"
+
+// Compute Scrabble score given a word string argument
 func Score(word string) int {
-	panic("Please implement the Score function")
+	score := 0
+	for _, letter := range strings.ToUpper(word) {
+		switch letter {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score++
+		case 'D', 'G':
+			score = score + 2
+		case 'B', 'C', 'M', 'P':
+			score = score + 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score = score + 4
+		case 'K':
+			score = score + 5
+		case 'J', 'X':
+			score = score + 8
+		case 'Q', 'Z':
+			score = score + 10
+		}
+	}
+	return score
 }
