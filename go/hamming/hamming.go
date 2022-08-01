233be7aeaca26bf4173df5ca13b599b3de2eb416
Chip Castle <chip@chipcastle.com>
2022-08-01 07:19:02 -0500

    [Go] Add solution to hamming exercise

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,18 @@
+// Package hamming calculates the Hamming Distance between two DNA strands
 package hamming
 
+import "errors"
+
+// Distance accepts 2 DNA strand string arguments and returns the hamming distance
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("cannot calculate hamming distance because strings provided are of unequal length")
+	}
+	count := 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			count++
+		}
+	}
+	return count, nil
 }
