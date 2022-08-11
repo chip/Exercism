09056477de95b7785db22aab9d0541bc91964154
Chip Castle <chip@chipcastle.com>
2022-08-11 13:09:44 +0200

    [Go] Add comments to blackjack exercise before marking as completed

--- go/blackjack/blackjack.go
+++ go/blackjack/blackjack.go
@@ -33,15 +33,20 @@ func ParseCard(card string) int {
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
 	sum := ParseCard(card1) + ParseCard(card2)
+	// Split when player has 2 aces
 	if sum == 22 {
 		return "P"
 	}
 	dealerSum := ParseCard(dealerCard)
+	// Hold when player has score less than or equal to 11
+	// Hold when player has score between 12 and 16 and dealer has score of at least 7
 	if sum <= 11 || (sum >= 12 && sum <= 16 && dealerSum >= 7) {
 		return "H"
 	}
+	// Automatically win when player has blackjack and dealer has score of less than 10
 	if sum == 21 && dealerSum < 10 {
 		return "W"
 	}
+	// Default tactic is to Stay
 	return "S"
 }
