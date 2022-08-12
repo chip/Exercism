bfa3f72bab6feb21d06b471fb05c817e1afec2bd
Chip Castle <chip@chipcastle.com>
2022-08-12 19:00:55 +0200

    [Go] Add solution for lasagna-master exercise

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,49 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+// Calculate preparation time by multiplying the number of layers by the averagePrepTimePerLayer
+// Set a default of 2 if avgPrepTimePerLayer arg is 0 to ensure a positive time returned
+func PreparationTime(layers []string, avgPrepTimePerLayer int) int {
+	if avgPrepTimePerLayer == 0 {
+		avgPrepTimePerLayer = 2
+	}
+	return len(layers) * avgPrepTimePerLayer
+}
 
-// TODO: define the 'Quantities()' function
+// Calculate the amount of noodles in grams (int) and sauce in liters (float64) for layers arg
+func Quantities(layers []string) (int, float64) {
+	noodles := 0
+	sauce := 0.0
+	for _, layer := range layers {
+		if layer == "noodles" {
+			noodles += 50
+		}
+		if layer == "sauce" {
+			sauce += 0.2
+		}
+	}
+	return noodles, sauce
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+// Fetch the last item from the friends ingredient list and set it as the last item of my ingredients
+func AddSecretIngredient(friendsList []string, myList []string) {
+	lastItemIndex := len(friendsList) - 1
+	secretIngredient := friendsList[lastItemIndex]
+	lastItemIndex = len(myList) - 1
+	myList[lastItemIndex] = secretIngredient
+}
 
-// TODO: define the 'ScaleRecipe()' function
+// The amounts listed in your cookbook only yield enough lasagna for two portions.
+// Since you want to cook for more people next time, you want to calculate the
+// amounts for different numbers of portions.
+//
+// Return revised amounts that includes the elements scaled according to their
+// factor (i.e., portions / 2)
+func ScaleRecipe(amounts []float64, portions int) []float64 {
+	servingsPerAmount := float64(2)
+	factor := float64(portions) / servingsPerAmount
+	scaledAmounts := make([]float64, len(amounts))
+	for i, amount := range amounts {
+		scaledAmounts[i] = amount * factor
+	}
+	return scaledAmounts
+}
