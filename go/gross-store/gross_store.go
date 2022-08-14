8d85b0bcc333c31919fe8b7ce3c553b50a4e284a
Chip Castle <chip@chipcastle.com>
2022-08-14 09:56:12 +0200

    [Go] Refactor solution for gross-store exercise (after reviewing community solutions)

--- go/gross-store/gross_store.go
+++ go/gross-store/gross_store.go
@@ -19,45 +19,43 @@ func NewBill() map[string]int {
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	measurement := units[unit]
-	if measurement <= 0 {
-		return false
+	measurement, exists := units[unit]
+	if exists {
+		bill[item] += measurement
+		return true
 	}
-	_, ok := bill[item]
-	if !ok {
-		bill[item] = 0
-	}
-	bill[item] += measurement
-	return true
+	return false
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	qty, ok := bill[item]
-	if !ok {
+	// Return false if the given unit is not in the units map.
+	measurement, exists := units[unit]
+	if !exists {
 		return false
 	}
-	measurement, ok := units[unit]
-	if !ok {
+	// Return false if the given item is not in the bill
+	qty, exists := bill[item]
+	if !exists {
 		return false
 	}
+	// Return false if the new quantity would be less than 0.
 	newQty := qty - measurement
 	if newQty < 0 {
 		return false
 	}
+	// If the new quantity is 0, completely remove the item from the bill then return true.
 	if newQty == 0 {
 		delete(bill, item)
 		return true
 	}
+	// Otherwise, reduce the quantity of the item and return true.
 	bill[item] -= measurement
 	return true
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	amount, ok := bill[item]
-	if !ok {
-		return 0, false
-	}
-	return amount, true
+	qty, exists := bill[item]
+	return qty, exists
 }
