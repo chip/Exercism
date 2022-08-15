7de69cd0a28f8bfb0aaeac02435c4723798d7e56
Chip Castle <chip@chipcastle.com>
2022-08-15 17:29:52 +0200

    [Go] Add solution for elons-toys exercise

--- go/elons-toys/elons_toys.go
+++ go/elons-toys/elons_toys.go
@@ -1,9 +1,23 @@
 package elon
 
-// TODO: define the 'Drive()' method
+import "fmt"
 
-// TODO: define the 'DisplayDistance() string' method
+func (car Car) Drive() Car {
+	if car.battery >= car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+	return car
+}
 
-// TODO: define the 'DisplayBattery() string' method
+func (car Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", car.distance)
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+func (car Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", car.battery)
+}
+
+func (car Car) CanFinish(trackDistance int) bool {
+	return car.battery >= ((trackDistance / car.speed) * car.batteryDrain)
+}
