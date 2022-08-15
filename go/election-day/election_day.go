359fbd178721ccb7803d733207a95d91febf3346
Chip Castle <chip@chipcastle.com>
2022-08-15 17:01:52 +0200

    [Go] Refactor solution for election-day exercise (after reviewing community solutions)

--- go/election-day/election_day.go
+++ go/election-day/election_day.go
@@ -5,8 +5,7 @@ import "fmt"
 // NewVoteCounter returns a new vote counter with
 // a given number of initial votes.
 func NewVoteCounter(initialVotes int) *int {
-	voteCount := initialVotes
-	return &voteCount
+	return &initialVotes
 }
 
 // VoteCount extracts the number of votes from a counter.
@@ -24,20 +23,15 @@ func IncrementVoteCount(counter *int, increment int) {
 
 // NewElectionResult creates a new election result.
 func NewElectionResult(candidateName string, votes int) *ElectionResult {
-	return &ElectionResult{Name: candidateName, Votes: votes}
+	return &ElectionResult{candidateName, votes}
 }
 
 // DisplayResult creates a message with the result to be displayed.
 func DisplayResult(result *ElectionResult) string {
-	er := *result
-	return fmt.Sprintf("%s (%d)", er.Name, er.Votes)
+	return fmt.Sprintf("%s (%d)", result.Name, result.Votes)
 }
 
 // DecrementVotesOfCandidate decrements by one the vote count of a candidate in a map.
 func DecrementVotesOfCandidate(results map[string]int, candidate string) {
-	for name, votes := range results {
-		if candidate == name {
-			results[candidate] = votes - 1
-		}
-	}
+	results[candidate]--
 }
