ba5a50b40e6548d59eebd9629e8191cd55f1ddaa
Chip Castle <chip@chipcastle.com>
2022-08-21 12:40:40 +0200

    [Go] Add solution to meteorology exercise

--- go/meteorology/meteorology.go
+++ go/meteorology/meteorology.go
@@ -1,5 +1,7 @@
 package meteorology
 
+import "fmt"
+
 type TemperatureUnit int
 
 const (
@@ -8,6 +10,12 @@ const (
 )
 
 // Add a String method to the TemperatureUnit type
+func (tu TemperatureUnit) String() string {
+	if tu == 0 {
+		return "°C"
+	}
+	return "°F"
+}
 
 type Temperature struct {
 	degree int
@@ -15,6 +23,9 @@ type Temperature struct {
 }
 
 // Add a String method to the Temperature type
+func (t Temperature) String() string {
+	return fmt.Sprintf("%d %s", t.degree, t.unit.String())
+}
 
 type SpeedUnit int
 
@@ -24,6 +35,12 @@ const (
 )
 
 // Add a String method to SpeedUnit
+func (su SpeedUnit) String() string {
+	if su == 0 {
+		return "km/h"
+	}
+	return "mph"
+}
 
 type Speed struct {
 	magnitude int
@@ -31,6 +48,9 @@ type Speed struct {
 }
 
 // Add a String method to Speed
+func (sp Speed) String() string {
+	return fmt.Sprintf("%d %s", sp.magnitude, sp.unit.String())
+}
 
 type MeteorologyData struct {
 	location      string
@@ -41,3 +61,8 @@ type MeteorologyData struct {
 }
 
 // Add a String method to MeteorologyData
+// Output: San Francisco: 57 °F, Wind NW at 19 mph, 60% Humidity
+func (m MeteorologyData) String() string {
+	s := "%s: %s, Wind %s at %s, %d%% Humidity"
+	return fmt.Sprintf(s, m.location, m.temperature.String(), m.windDirection, m.windSpeed.String(), m.humidity)
+}
