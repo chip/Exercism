6a3903b62e0888780adde480085886999bfe8684
Chip Castle <chip@chipcastle.com>
2022-08-21 12:02:13 +0200

    [Go] Add solution to parsing-log-files exercise

--- go/parsing-log-files/parsing_log_files.go
+++ go/parsing-log-files/parsing_log_files.go
@@ -1,21 +1,44 @@
 package parsinglogfiles
 
+import (
+	"regexp"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	re := regexp.MustCompile(`^\[(TRC|DBG|INF|WRN|ERR|FTL)\]`)
+	return re.Match([]byte(text))
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	return regexp.MustCompile(`\<[~*=-]*\>`).Split(text, -1)
 }
 
 func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+	re := regexp.MustCompile(`(?i)(".*password"?)`)
+	sum := 0
+	for _, line := range lines {
+		if re.Match([]byte(line)) {
+			sum++
+		}
+	}
+	return sum
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	old := `end-of-line\d+`
+	re := regexp.MustCompile(old)
+	if re.Match([]byte(text)) {
+		return re.ReplaceAllString(text, "")
+	}
+	return text
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	re := regexp.MustCompile(`User\s+(\w+)`)
+	for i, line := range lines {
+		if matches := re.FindSubmatch([]byte(line)); matches != nil {
+			lines[i] = "[USR] " + string(matches[1]) + " " + line
+		}
+	}
+	return lines
 }
