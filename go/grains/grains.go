2376627584a26106d13bcc9a760b52ab75f6f220
Chip Castle <chip@chipcastle.com>
2022-08-22 09:15:53 +0200

    [Go] Add solution to grains exercise

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,26 @@
 package grains
 
+import (
+	"errors"
+	"math"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number <= 0 || number > 64 {
+		return 0, errors.New("number must be between 1-64")
+	}
+	n := math.Pow(2, float64(number-1))
+	return uint64(n), nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	var sum uint64 = 0
+
+	for i := 1; i <= 64; i++ {
+		n, err := Square(i)
+		if err == nil {
+			sum += n
+		}
+	}
+	return sum
 }
