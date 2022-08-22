7e6059f9276077266a21a3cd892cb0549d9f5574
Chip Castle <chip@chipcastle.com>
2022-08-22 09:31:11 +0200

    [Go] Refactor solution to grains exercise

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,25 +2,21 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
-func Square(number int) (uint64, error) {
-	if number <= 0 || number > 64 {
-		return 0, errors.New("number must be between 1-64")
+const chessSquares int = 64
+
+func Square(x int) (grains uint64, err error) {
+	if x < 1 || x > chessSquares {
+		err = errors.New("number must be between 1-64")
 	}
-	n := math.Pow(2, float64(number-1))
-	return uint64(n), nil
+	return 1 << uint64(x-1), err
 }
 
-func Total() uint64 {
-	var sum uint64 = 0
-
-	for i := 1; i <= 64; i++ {
-		n, err := Square(i)
-		if err == nil {
-			sum += n
-		}
+func Total() (total uint64) {
+	for i := 1; i <= chessSquares; i++ {
+		n, _ := Square(i)
+		total += n
 	}
-	return sum
+	return total
 }
