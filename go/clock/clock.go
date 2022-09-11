aca91ebe23c191644a4ccea9c835daa5db7c633a
Chip Castle <chip@chipcastle.com>
2022-09-11 10:14:10 -0500

    [Go] Add WIP for clock exercise

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -1,19 +1,49 @@
 package clock
 
+import (
+	"fmt"
+	"math"
+)
+
 // Define the Clock type here.
+type Clock struct {
+	hour   int
+	minute int
+}
 
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	if m >= 60 {
+		tmpClock := convertMinutes(m)
+
+		if tmpClock.hour > 0 {
+			h += tmpClock.hour
+		}
+		m += tmpClock.minute
+	}
+	return Clock{hour: h, minute: m}
 }
 
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	c.minute += m
+	return c
 }
 
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	c.minute -= m
+	return c
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	hour := c.hour % 24
+	// if c.hour >= 24 {
+	// 	hour = c.hour % 24
+	// }
+	return fmt.Sprintf("%02d:%02d", hour, c.minute)
+}
+
+func convertMinutes(m int) Clock {
+	minutesPerHour := 60.0
+	hours := int(math.Mod(float64(m), minutesPerHour))
+	minutes := int(math.Remainder(float64(m), minutesPerHour))
+	return Clock{hour: hours, minute: minutes}
 }
