45916597130c440b18d3ee3589e7df0618061e95
Chip Castle <chip@chipcastle.com>
2022-10-29 16:48:00 -0500

    [Racket] Refactor based upon community solutions for leap exercise

--- racket/leap/leap.rkt
+++ racket/leap/leap.rkt
@@ -3,7 +3,8 @@
 (provide leap-year?)
 
 (define (leap-year? year)
-  (cond ((zero? (remainder year 400)) #t)
-        ((zero? (remainder year 100)) #f)
-        ((zero? (remainder year 4)) #t)
-        (else #f)))
+  (cond
+    [(zero? (remainder year 400)) #t]
+    [(zero? (remainder year 100)) #f]
+    [(zero? (remainder year 4)) #t]
+    [else #f]))
