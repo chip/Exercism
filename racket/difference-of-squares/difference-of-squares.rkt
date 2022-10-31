915f30334e31ae3ee702d4e77c143baf3102f94d
Chip Castle <chip@chipcastle.com>
2022-10-31 16:11:07 -0500

    [Racket] Add solution for difference-of-squares exercise

--- racket/difference-of-squares/difference-of-squares.rkt
+++ racket/difference-of-squares/difference-of-squares.rkt
@@ -2,11 +2,17 @@
 
 (provide sum-of-squares square-of-sum difference)
 
+(define (number-range number)
+  (range 1 (add1 number)))
+  
 (define (sum-of-squares number)
-  (error "Not implemented yet"))
+  (for/sum ([i (number-range number)]) (* i i)))
+
+(define (sum-of number)
+  (for/sum ([i (number-range number)]) i))
 
 (define (square-of-sum number)
-  (error "Not implemented yet"))
+  (sqr (sum-of number)))
 
 (define (difference number)
-  (error "Not implemented yet"))
+  (- (square-of-sum number) (sum-of-squares number)))
