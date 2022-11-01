dc0a68695c756052ecbfd2b8eab17b79616491d5
Chip Castle <chip@chipcastle.com>
2022-11-01 18:02:08 -0500

    [Racket] Refactor based upon community solutions for bob exercise

--- racket/bob/bob.rkt
+++ racket/bob/bob.rkt
@@ -2,11 +2,26 @@
 
 (provide response-for)
 
+(define (silence? prompt)
+  (regexp-match #rx"^[ ]*$" prompt))
+
+(define (shouting? prompt)
+  (regexp-match #rx"^[A-Z0-9 %^*@#$(),!]+!?$" prompt))
+
+(define (shouting-question? prompt)
+  (regexp-match #rx"^[A-Z ]+\\?$" prompt))
+
+(define (question? prompt)
+  (regexp-match #rx"\\?$" prompt))
+
+(define (only-numbers? prompt)
+  (regexp-match #rx"^[0-9 ,]+$" prompt))
+
 (define (response-for prompt)
   (cond 
-    [(regexp-match #rx"^[ ]*$" prompt) "Fine. Be that way!"]
-    [(regexp-match #rx"^[0-9 ,]+$" prompt) "Whatever."]
-    [(regexp-match #rx"^[A-Z0-9 %^*@#$(),!]+!?$" prompt) "Whoa, chill out!"]
-    [(regexp-match #rx"^[A-Z ]+\\?$" prompt) "Calm down, I know what I'm doing!"]
-    [(regexp-match #rx"\\?$" prompt) "Sure."]
+    [(silence? prompt) "Fine. Be that way!"]
+    [(only-numbers? prompt) "Whatever."]
+    [(shouting? prompt) "Whoa, chill out!"]
+    [(shouting-question? prompt) "Calm down, I know what I'm doing!"]
+    [(question? prompt) "Sure."]
     [else "Whatever."]))
