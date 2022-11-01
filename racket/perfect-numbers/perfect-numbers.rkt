6d67c4bfcafcb3a4ac8f5b7f0528ed30c91f41b5
Chip Castle <chip@chipcastle.com>
2022-11-01 14:14:46 -0500

    [Racket] Add solution for perfect-numbers exercise

--- racket/perfect-numbers/perfect-numbers.rkt
+++ racket/perfect-numbers/perfect-numbers.rkt
@@ -2,5 +2,16 @@
 
 (provide classify)
 
+(define (factors number)
+  (let ([numbers empty])
+    (for ([i (range 1 number)]
+          #:when (zero? (remainder number i)))
+      (set! numbers (append numbers (list i))))
+    numbers))
+
 (define (classify number)
-  (error "Not implemented yet"))
+  (let ([x (for/sum ([i (factors number)]) i)])
+    (cond
+      [(= x number) 'perfect]
+      [(< x number) 'deficient]
+      [(> x number) 'abundant])))
