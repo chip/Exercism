5672bae20552bb5314bd09809a4f5f36118e1ab1
Chip Castle <chip@chipcastle.com>
2022-11-02 14:11:10 -0500

    [Racket] Add solution for hamming exercise

--- racket/hamming/hamming.rkt
+++ racket/hamming/hamming.rkt
@@ -3,4 +3,12 @@
 (provide hamming-distance)
 
 (define (hamming-distance source target)
-  (error "Not implemented yet"))
+  (cond 
+    [(= (string-length source) (string-length target))
+     (let ([distance 0])
+      (map
+        (lambda (x y)
+          (cond 
+            [(not (eq? x y)) (set! distance (add1 distance))]))
+        (string->list source) (string->list target)) distance)]
+    [else (error "String lengths are not equal")]))
