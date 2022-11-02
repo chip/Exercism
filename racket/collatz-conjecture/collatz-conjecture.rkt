c5c94b3080dd65de5b65f018815e6ff4772132e5
Chip Castle <chip@chipcastle.com>
2022-11-02 09:55:50 -0500

    [Racket] Add solution for collatz-conjecture exercise

--- racket/collatz-conjecture/collatz-conjecture.rkt
+++ racket/collatz-conjecture/collatz-conjecture.rkt
@@ -2,5 +2,19 @@
 
 (provide collatz)
 
+(define steps 0)
+
 (define (collatz num)
-  (error "Not implemented yet"))
+  (cond 
+    [(zero? num) (error exn:fail #t)]
+    [(negative? num) (error exn:fail #t)]
+    [(not (integer? num)) (error exn:fail #t)]
+    [(= 1 num) (let ([result steps])
+                 (set! steps 0)
+                 result)]
+    [else
+      (set! steps (add1 steps))
+      (cond
+        [(even? num) (collatz (/ num 2))]
+        [(odd? num) (collatz (add1 (* num 3)))]
+        [else (error exn:fail #t)])]))
