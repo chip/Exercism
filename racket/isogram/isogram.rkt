fb82336cea9d365ae121939da1373f84183625d6
Chip Castle <chip@chipcastle.com>
2022-11-04 16:19:43 -0500

    [Racket] Add solution for isogram exercise

--- racket/isogram/isogram.rkt
+++ racket/isogram/isogram.rkt
@@ -2,5 +2,9 @@
 
 (provide isogram?)
 
-(define (isogram? s)
-  (error "Not implemented yet"))
+(define (special-char? byte)
+  (not (member byte '(#\space #\-))))
+       
+(define (isogram? word)
+  (let ([letters (filter special-char? (string->list (string-downcase word)))])
+    (= (length letters) (length (remove-duplicates letters)))))
