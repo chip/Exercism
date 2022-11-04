b49a09fefb91412c45acae803046c4f92e2e5597
Chip Castle <chip@chipcastle.com>
2022-11-04 13:13:23 -0500

    [Racket] Add solution for scrabble-score exercise

--- racket/scrabble-score/scrabble-score.rkt
+++ racket/scrabble-score/scrabble-score.rkt
@@ -2,5 +2,19 @@
 
 (provide score)
 
+(define (lookup byte)
+  (let ([letter (string-upcase (string byte))])
+    (cond 
+      [(regexp-match? #rx"[AEIOULNRST]" letter) 1]
+      [(regexp-match? #rx"[DG]" letter) 2]
+      [(regexp-match? #rx"[BCMP]" letter) 3]
+      [(regexp-match? #rx"[FHVWY]" letter) 4]
+      [(regexp-match? #rx"[K]" letter) 5]
+      [(regexp-match? #rx"[JX]" letter) 8]
+      [(regexp-match? #rx"[QZ]" letter) 10]
+      [else 0])))
+
 (define (score word)
-  (error "Not implemented yet"))
+  (if (zero? (string-length word))
+    0
+    (for/sum ([i (map (lambda (byte) (lookup byte)) (string->list word))]) i)))
