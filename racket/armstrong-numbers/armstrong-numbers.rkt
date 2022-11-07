94cf63d48b1f3be9198f8db5632e414eebbfca09
Chip Castle <chip@chipcastle.com>
2022-11-07 16:09:51 -0600

    [Racket] Add solution for armstrong-numbers exercise

--- racket/armstrong-numbers/armstrong-numbers.rkt
+++ racket/armstrong-numbers/armstrong-numbers.rkt
@@ -3,4 +3,10 @@
 (provide armstrong-number?)
 
 (define (armstrong-number? n)
-  (error "Not implemented yet"))
+  (= n (for/sum ([i (nums n)]) i)))
+
+(define (nums n)
+  (let* ([s (~v n)]
+         [w (string-length s)])
+    (for/list ([c (string->list s)])
+      (expt (- (char->integer c) 48) w))))
