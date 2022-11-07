af692f05b961fc2e63b5d6c103319a52872c2f1d
Chip Castle <chip@chipcastle.com>
2022-11-07 12:45:55 -0600

    [Racket] Add solution for robot-name exercise

--- racket/robot-name/robot-name.rkt
+++ racket/robot-name/robot-name.rkt
@@ -5,14 +5,34 @@
          reset!
          reset-name-cache!)
 
+(define ascii-offset-letters 65)
+(define ascii-offset-numbers 48)
+
+(define cache (make-hash))
+
+(define (letters)
+  (list->string
+    (for/list ([i (in-range 2)])
+      (integer->char (+ ascii-offset-letters (random 26))))))
+
+(define (numbers)
+  (list->string
+    (for/list ([i (in-range 3)])
+      (integer->char (+ ascii-offset-numbers (random 10))))))
+
+(define (create-name)
+  (string-append (letters) (numbers)))
+
 (define (make-robot)
-  (error "Not implemented yet"))
+  (let ([name (create-name)])
+    (hash-set! cache name name)
+    name))
 
 (define (name robot)
-  (error "Not implemented yet"))
+  (hash-ref cache robot))
 
 (define (reset! robot)
-  (error "Not implemented yet"))
+  (hash-remove! cache name))
 
 (define (reset-name-cache!)
-  (error "Not implemented yet"))
+  (hash-clear! cache))
