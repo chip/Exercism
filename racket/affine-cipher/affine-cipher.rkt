e63013d623f0bae8aa22fce78d3da7ab7ce4814b
Chip Castle <chip@chipcastle.com>
2022-11-10 15:59:04 -0600

    [Racket] Add WIP for affine-cipher exercise

--- racket/affine-cipher/affine-cipher.rkt
+++ racket/affine-cipher/affine-cipher.rkt
@@ -1,5 +1,7 @@
 #lang racket/base
 (require racket/contract)
+(require racket/string)
+(require racket/list)
 
 (provide (contract-out
           [encode (string?
@@ -9,8 +11,37 @@
                    exact-nonnegative-integer?
                    exact-nonnegative-integer? . -> . string?)]))
 
+(define n-chars 5)
+(define m 26)
+(define ascii-offset 97)
+(define fmt "c: ~v x: ~v z: ~v")
+
+; `E(x) = (ax + b) mod m`)
+(define (Ex c a b)
+  (let* ([i (char->integer c)]
+         [x (- i ascii-offset)]
+         [p (+ (* a x) b)]
+         [r (remainder p m)]
+         [z (integer->char (+ r ascii-offset))])
+    ;(displayln (format fmt c x z))
+    (if (exact-positive-integer? x)
+      z
+      c))) 
+  
+(define (add-whitespace lst)
+  (if (>= (length lst) n-chars)
+    (flatten (list (take lst n-chars) #\space (add-whitespace (drop lst n-chars))))
+    lst))
+        
+;(add-whitespace (string->list "mindblowingly"))
+
 (define (encode msg a b)
-  (error "Not implemented yet"))
+  (list->string
+    (add-whitespace
+      (map
+        (lambda (c)
+          (Ex c a b))
+        (filter char-alphabetic? (string->list (string-downcase msg)))))))
 
 (define (decode msg a b)
   (error "Not implemented yet"))
