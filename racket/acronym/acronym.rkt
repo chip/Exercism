9d146672398db3a8945db1adfc6df49d8166a75e
Chip Castle <chip@chipcastle.com>
2022-11-14 08:42:23 -0600

    [Racket] Add solution for acronym exercise

--- racket/acronym/acronym.rkt
+++ racket/acronym/acronym.rkt
@@ -2,5 +2,14 @@
 
 (provide acronym)
 
-(define (acronym string)
-  (error "Not implemented yet"))
+(define (sanitize s)
+  (string-upcase (string-replace s #rx"[-_]" " ")))
+
+(define (words s)
+  (string-split s " "))
+
+(define (acronym s)
+  (string-join (map (lambda (word)
+                      (if (zero? (string-length word))
+                        ""
+                        (substring word 0 1))) (words (sanitize s))) ""))
