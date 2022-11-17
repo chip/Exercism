63d04963dfe76a1fe805006ad7c85d22d2f80149
Chip Castle <chip@chipcastle.com>
2022-11-17 14:33:19 -0600

    [Racket] Add WIP for base10->b2 in all-your-base exercise

--- racket/all-your-base/all-your-base.rkt
+++ racket/all-your-base/all-your-base.rkt
@@ -5,54 +5,82 @@
 (define (invalid-arguments? b1 b2)
   (or (< b1 2) (<= b2 1) (negative? b2)))
 
+(define (format-list result b2)
+  (displayln (format "format-list [result ~v] [b2 ~v]" result b2))
+  (if (= b2 10)
+    (list (for/sum ([i result]) i))
+    (filter-map string->number (regexp-split #rx"" (number->string (first result))))))
+
+
+(define (to-base-10 list-digits b1 b2)
+  (let process ([lst (reverse list-digits)]
+                [position 0]
+                [result '()])
+    ;(displayln (format "[lst: ~v] [position: ~v] [result: ~v]" lst position result))
+    (if (empty? lst)
+      ;(list result)
+      ;result
+      (format-list result b2)
+      (let ([digit (car lst)])
+        (displayln (format "[digit ~v] [lst ~v] [result ~v] [list? result ~v]" digit lst result (list? result)))
+        (if (or (>= digit b1) (negative? digit))
+          #f
+          ;(process (cdr lst) (add1 position) (append result (list (* digit (expt b1 position))))))))))))
+          (if (positive? digit)
+            (process (cdr lst) (add1 position) (append result (list (* digit (expt b1 position)))))
+            (process (cdr lst) (add1 position) result)))))))
+  
+(define (list->number lst)
+  (string->number (string-join (map ~a lst) "")))
+
+; 2 5
+;   2 R1
+;   1 R0 
+;divide n by b2 successively
+(define (base10->b2 list-digits b2)
+  (let ([z (list->number list-digits)])
+    ;(let-values process ([(q r) (quotient/remainder n b2)]))
+    (let process ([n z]
+                  [remainders empty])
+      (let* ([q (quotient n b2)]
+             [r (remainder n b2)])
+        (displayln (format "n ~v | q ~v | r ~v | remainders ~v" n q r remainders))
+        (if (zero? q)
+          (append remainders (list r))
+          (process q (append remainders (list r))))))))
+
+;(base10->b2 '( 5) 2)
+
 (define (rebase list-digits b1 b2)
+  (if (invalid-arguments? b1 b2)
+    #f
+    (if (empty? list-digits)
+      '( 0)
+      (cond [(= b1 10) (base10->b2 list-digits b2)]
+            [(= b2 10) (to-base-10 list-digits b1 b2)]
+            [else (error 'whooops)]))))
+    
+
+;(rebase '( 5 ) 10 2)
+(define (___rebase list-digits b1 b2)
   (if (invalid-arguments? b1 b2)
     #f
     (if (empty? list-digits)
       '( 0)
       (let process ([lst (reverse list-digits)]
                     [position 0]
-                    [result empty])
-        (displayln (format "[lst: ~v] [position: ~v] [result: ~v]" lst position result))
+                    [result '()])
+        ;(displayln (format "[lst: ~v] [position: ~v] [result: ~v]" lst position result))
         (if (empty? lst)
-          (list result)
+          ;(list result)
+          ;result
+          (format-list result b2)
           (let ([digit (car lst)])
+            (displayln (format "[digit ~v] [lst ~v] [result ~v] [list? result ~v]" digit lst result (list? result)))
             (if (or (>= digit b1) (negative? digit))
               #f
+              ;(process (cdr lst) (add1 position) (append result (list (* digit (expt b1 position))))))))))))
+
               (if (positive? digit)
-                (process (cdr lst) (add1 position) (append result (* digit (expt b1 position))))
-                (process (cdr lst) (add1 position) (append result empty))))))))))
-    
-(rebase '( 0 6 0 ) 7 10)
-;(test-equal? "leading-zeros"
-;             '( 4 2)
-;             (rebase '( 0 6 0 ) 7 10))))))))
-
-;(define (_rebase list-digits b1 b2)
-;  (if (or (empty? list-digits) (negative? b1) (negative? b2))
-;    #f
-;    (let go ([result empty]
-;             [lst (reverse list-digits)])
-;      ;(displayln (format "result: ~v" result))
-;      ;(displayln (format "lst ~v" lst))
-;      (if (empty? lst)
-;        (reverse (list result))
-;        (let* ([n (car lst)]
-;               [rem (remainder n b2)])
-;          (go (list (append (list result) rem)) (cdr lst)))))))
-;
-; Convert to base 10 first
-; Convert result by dividing by b2, saving remainder (last remainder is first-most digit in result)
-
-; Convert   '( 1 0 1 ) b1=2, b2=10
-; Position:    2 1 0
-; (1 * (b1 ^ 2)) + (0 * (b1 ^ 1)) + (1 * (b1 ^ 0))
-; (1 * (2  * 2)) + (0 * (2)) + (1 * (1))
-; (1 * 4)        + (0 * 2)   + (1 * 1))
-; 4              + 0              + 1
-; 5
-
-;(rebase '( 1 0 1 ) 2 10)
-;     (test-equal? "binary-to-single-decimal"
-;                  '( 5)
-;                  (rebase '( 1 0 1 ) 2 10)))
+                (process (cdr lst) (add1 position) (append result (list (* digit (expt b1 position)))))
+                (process (cdr lst) (add1 position) result)))))))))
