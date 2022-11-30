9b1d768c97f97d8b6495f0ad57591cc9c4368402
Chip Castle <chip@chipcastle.com>
2022-11-30 09:27:21 -0600

    [Racket] Add solution for allergies exercise

--- racket/allergies/allergies.rkt
+++ racket/allergies/allergies.rkt
@@ -2,44 +2,28 @@
 
 (provide list-allergies allergic-to?)
 
-(define allergy-names (hash
-                        "eggs" 1
-                        "peanuts" 2
-                        "shellfish" 4
-                        "strawberries" 8
-                        "tomatoes" 16
-                        "chocolate" 32
-                        "pollen" 64
-                        "cats" 128))
-
 (define (find-allergy score)
-  (cond [(>= score 128) "cats"]
-        [(>= score  64) "pollen"]
-        [(>= score  32) "chocolate"]
-        [(>= score  16) "tomatoes"]
-        [(>= score   8) "strawberries"]
-        [(>= score   4) "shellfish"]
-        [(>= score   2) "peanuts"]
-        [(>= score   1) "eggs"]
-        [else #f]))
+  (cond [(>= score 128) (cons 128 "cats")]
+        [(>= score  64) (cons 64  "pollen")]
+        [(>= score  32) (cons 32  "chocolate")]
+        [(>= score  16) (cons 16  "tomatoes")]
+        [(>= score   8) (cons 8   "strawberries")]
+        [(>= score   4) (cons 4   "shellfish")]
+        [(>= score   2) (cons 2   "peanuts")]
+        [(>= score   1) (cons 1   "eggs")]
+        [else (cons 0 #f)]))
 
-(define (normalize-score score)
-  (if (> score 256)
-    (modulo score 256)
-    score))
+(define threshold 256)
 
-(define (list-allergies orig-score)
+(define (list-allergies original-score)
   (let process ([lst empty]
-                [score (normalize-score orig-score)])
-    (if (zero? score)
-      lst
-      (let ([allergy (find-allergy score)])
-        (if (not allergy)
-          lst
-          (let ([allergy-score (hash-ref allergy-names allergy)])
-            (if (zero? allergy-score)
-              lst
-              (process (append lst (list allergy)) (- score allergy-score)))))))))
+                [score (modulo original-score threshold)])
+    (let* ([allergy-pair (find-allergy score)]
+           [allergy-score (car allergy-pair)]
+           [allergy (cdr allergy-pair)])
+      (if (or (zero? allergy-score) (not allergy))
+        lst
+        (process (append lst (list allergy)) (- score allergy-score))))))
 
 (define (allergic-to? str score)
   (let ([lst (list-allergies score)])
