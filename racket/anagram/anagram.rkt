cc4390f93b92e231f5aaa81bb0e24b176d1d7c5e
Chip Castle <chip@chipcastle.com>
2022-12-05 16:23:39 -0600

    [Racket] Add solution for anagram exercise

--- racket/anagram/anagram.rkt
+++ racket/anagram/anagram.rkt
@@ -2,5 +2,17 @@
 
 (provide anagrams-for)
 
-(define (anagrams-for string possibilities)
-  (error "Not implemented yet"))
+(define (frequencies s)
+  (define table (make-hash))
+  (for/list ([i (string->list (string-downcase s))])
+    (hash-update! table i add1 0))
+  table)
+
+(define (anagram? s p)
+  (cond [(not (= (string-length s) (string-length p))) #f]
+        [(string-ci=? s p) #f]
+        [(equal? (frequencies s) (frequencies p)) p]
+        [else #f]))
+
+(define (anagrams-for candidate possibilities)
+  (filter string? (map (lambda (p) (anagram? candidate p)) possibilities)))
