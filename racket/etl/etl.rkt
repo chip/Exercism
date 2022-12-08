53622826305f5f11c7314dd267deee9ace5c9ec3
Chip Castle <chip@chipcastle.com>
2022-12-07 21:44:30 -0600

    [Racket] Add WIP for etl exercise

--- racket/etl/etl.rkt
+++ racket/etl/etl.rkt
@@ -2,5 +2,37 @@
 
 (provide etl)
 
+(define scores (hash 'a 1 'e 1 'i 1 'o 1 'u 1 'l 1 'n 1 'r 1 's 1 't 1
+                      'd 2 'g 2
+                      'b 3 'c 3 'm 3 'p 3
+                      'f 4 'h 4 'v 4 'w 4 'y 4
+                      'k 5
+                      'j 8 'x 8
+                      'q 10 'z 10))
+
+(hash-ref scores (string-ref "z" 0))
+
+(define (negative-keys? input)
+  (not (empty? (filter negative-integer? (hash-keys input)))))
+
+(define (non-string-values? input)
+  (empty? (filter string? (first (hash-values input)))))
+
+(define (hash->strings input)
+  (map string-downcase (filter string? (flatten (hash-values input)))))
+
+(define (convert input)
+  (let ([h (make-hash)])
+    (for ([s (hash->strings input)])
+      (let* ([x (string-ref s 0)]
+             [score (hash-ref scores x)])
+        (printf "x: ~a\n" x)
+        (hash-update! h x score)))
+    h))
+
 (define (etl input)
-  (error "Not implemented yet"))
+  (if (hash-empty? input)
+    (make-hash)
+    (cond [(negative-keys? input) (raise-argument-error 'negative-keys-input)]
+          [(non-string-values? input) (raise-argument-error 'non-string-values)]
+          [else (convert input)])))
