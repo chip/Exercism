a8ee193ac20070709381960f6e0a36c079d26acf
Chip Castle <chip@chipcastle.com>
2022-12-08 14:04:01 -0600

    [Racket] Add refactored solution (with contracts) for etl exercise

--- racket/etl/etl.rkt
+++ racket/etl/etl.rkt
@@ -1,6 +1,8 @@
 #lang racket
 
-(provide etl)
+(provide
+  (contract-out
+    [etl (-> (hash/c positive? (listof string?)) (hash/c string? positive?))]))
 
 (define scores (hash "a" 1 "e" 1 "i" 1 "o" 1 "u" 1 "l" 1 "n" 1 "r" 1 "s" 1 "t" 1
                      "d" 2 "g" 2
@@ -10,22 +12,9 @@
                      "j" 8 "x" 8
                      "q" 10 "z" 10))
 
-(define (negative-keys? input)
-  (not (empty? (filter negative-integer? (hash-keys input)))))
-
-(define (non-string-values? input)
-  (empty? (filter string? (first (hash-values input)))))
-
 (define (hash->strings input)
   (map string-downcase (filter string? (flatten (hash-values input)))))
 
-(define (convert input)
+(define (etl input)
   (for/hash ([s (hash->strings input)])
     (values s (hash-ref scores s))))
-
-(define (etl input)
-  (if (hash-empty? input)
-    (make-hash)
-    (cond [(negative-keys? input) (raise-argument-error 'negative-keys-input)]
-          [(non-string-values? input) (raise-argument-error 'non-string-values)]
-          [else (convert input)])))
