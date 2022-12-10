4f1742c2edc8c37b278277949f15d7a80a34842e
Chip Castle <chip@chipcastle.com>
2022-12-10 09:52:57 -0600

    [Racket] Add WIP (13 passing tests) for grep exercise

--- racket/grep/grep.rkt
+++ racket/grep/grep.rkt
@@ -10,17 +10,30 @@
   (define (case-insensitive? flags)
     (member "-i" flags))
 
+  (define (match-line? flags)
+    (member "-x" flags))
+
+  (define (invert-match? flags)
+    (member "-v" flags))
+
+  (define (show-filename? flags)
+    (member "-l" flags))
+
   (define (case-i-re pattern)
-    (printf (pregexp (string-append pattern "i")))
-    (pregexp (string-append pattern "i")))
+    (pregexp (string-append "(?i:" pattern ")")))
+
+  (define (line-re pattern)
+    (pregexp (string-append "^" pattern "$")))
 
   (define (re flags pattern)
     (cond [(case-insensitive? flags) (case-i-re pattern)]
+          [(match-line? flags) (line-re pattern)]
           [else (regexp pattern)]))
 
-  (define (display-line flags line n)
+  (define (display-line flags file line n)
     ;(printf "line: ~a\n" line)
     (cond [(show-line-numbers? flags) (format "~a:~a" n line)]
+          [(show-filename? flags) file]
           [else line]))
 
   (let loop ([files files])
@@ -31,11 +44,11 @@
       (for/list ([line (in-list lines)]
                  [n (in-naturals 1)]
                  #:when (regexp-match (re flags pattern) line))
-        (display-line flags line n)))))
+        (display-line flags file line n)))
+    (when (not (empty? (cdr files)))
+      (loop (cdr files)))))
 
-;     (test-equal? "Multiple files, no matches, various flags"
-;                  (grep '("-n" "-l" "-x" "-i")
-;                        "Frodo"
-;                        '("iliad.txt" "midsummer-night.txt" "paradise-lost.txt"))
-;                  '())))
-
+#| (cond [(regexp-match (re flags pattern) line) (display-line flags file line n)]) |#
+#|       [else] |#
+#|         (when (invert-match? flags)) |#
+#|           (display-line flags file line n) |#
