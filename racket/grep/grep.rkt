a15d403f6414224a5c58ab0467a2bb3bc7ff7c8c
Chip Castle <chip@chipcastle.com>
2022-12-14 10:32:40 -0600

    [Racket] Add WIP (only 1 failing test) for grep exercise

--- racket/grep/grep.rkt
+++ racket/grep/grep.rkt
@@ -19,10 +19,12 @@
     (> (length files) 0))
  
   (define (show-filename? flags)
-    (or (member "-l" flags) (multiple-files? flags)))
+    (member "-l" flags))
 
   (define (regex pattern flags)
-    (cond [(case-insensitive? flags) (regexp (string-append "(?i:" pattern ")"))]
+    (cond 
+          [(and (case-insensitive? flags) (match-line? flags)) (regexp (string-append "^(?i:" pattern ")$"))]
+          [(case-insensitive? flags) (regexp (string-append "(?i:" pattern ")"))]
           [(match-line? flags) (regexp (string-append "^" pattern "$"))]
           [else (regexp pattern)]))
 
@@ -31,20 +33,47 @@
       (not (regexp-match (regex pattern flags) line))
       (regexp-match (regex pattern flags) line)))
 
-  (define (show-line flags file line n)
-    (cond [(multiple-files? flags) (format "~a" file)]
+  (define (format-line-single-file flags file line n)
+    ;(printf "s flags: ~a\n" flags)
+    (cond
           [(show-filename? flags) (format "~a" file)]
           [(show-line-numbers? flags) (format "~a:~a" n line)]
-          [else (format "else: ~a" line)]))
+          ;[(and (multiple-files? flags) (show-line-numbers? flags)) (format "~a:~a:~a" file n line)]
+          ;[(multiple-files? flags) (format "~a" line)]
+          [else (format "~a" line)]))
+
+  (define (format-line-multiple-files flags file line n)
+    ;(printf "mult flags: ~a\n" flags)
+    (cond
+          [(show-filename? flags) (format "~a" file)]
+          [(show-line-numbers? flags) (format "~a:~a:~a" file n line)]
+          ;[(and (multiple-files? flags) (show-line-numbers? flags)) (format "~a:~a:~a" file n line)]
+          ;[(multiple-files? flags) (format "~a" line)]
+          ;[else (format "~a" line)]
+          [else (format "~a:~a" file line)]))
+
+  (define (show-line flags file line n)
+    (cond
+          [(= 1 (length files)) (format-line-single-file flags file line n)]
+          [else (format-line-multiple-files flags file line n)]))
 
   (define (process file pattern flags)
     (define file-lines
       (file->lines (build-path (current-directory) file)))
-    (for/list ([line file-lines]
-               [n (range 1 (add1 (length file-lines)))]
-               #:when (show-line? line pattern flags))
-      (show-line flags file line n)))
+    (define matches empty)
+    (filter (lambda (s)
+              (not (void? s)))
+      (flatten
+        (map (lambda (line n)
+               (let ([ln (show-line flags file line n)])
+                 #| (printf (list->string matches)) |#
+                 #| (printf ln) |#
+                 ;(when (and (show-line? line pattern flags) (not (member ln matches))))
+                 (when (show-line? line pattern flags)
+                   ln)))
+             file-lines (range 1 (add1 (length file-lines)))))))
 
-  (flatten (map (lambda (file) (process file pattern flags)) files)))
-
-;(grep '("-x") "Eden" '("paradise-lost.txt"))
+  (flatten
+    (map (lambda (file)
+           (process file pattern flags))
+         files)))
