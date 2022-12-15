6b7ec6e28cdd4e361c550ac8fd14540d791f1b93
Chip Castle <chip@chipcastle.com>
2022-12-15 16:28:25 -0600

    [Racket] Add refactored solution for list-ops exercise

--- racket/list-ops/list-ops.rkt
+++ racket/list-ops/list-ops.rkt
@@ -9,47 +9,27 @@
          my-concatenate)
 
 (define (my-length sequence)
-  (let loop ([sequence sequence]
-             [sum 0])
-    (if (empty? sequence)
-      sum
-      (loop (rest sequence) (add1 sum))))) 
+  (for/sum ([_ sequence]) 1))
 
 (define (my-reverse sequence)
-  (let loop ([sequence sequence]
-             [lst empty])
-    (if (empty? sequence)
-      lst
-      (let ([n (first sequence)])
-        (loop (rest sequence) (append (list n) lst)))))) 
+  (my-fold cons '() sequence))
 
 (define (my-map operation sequence)
-  (let loop ([sequence sequence]
-             [lst empty])
-    (if (empty? sequence)
-      (flatten lst) 
-      (let* ([x (first sequence)]
-             [result (operation x)])
-        (loop (rest sequence) (append (list lst) (list result)))))))
+  (for/list ([i sequence])
+    (operation i)))
 
 (define (my-filter operation? sequence)
-  (let loop ([sequence sequence]
-             [lst empty])
-    (if (empty? sequence)
-      (flatten lst) 
-      (if (operation? (first sequence))
-        (loop (rest sequence) (append (list lst) (first sequence)))
-        (loop (rest sequence) (list lst))))))
+  (for/list ([i sequence]
+             #:when (operation? i))
+    i))
 
 (define (my-fold operation accumulator sequence)
-  (let loop ([accumulator accumulator]
-             [sequence sequence])
-    (if (empty? sequence)
-      accumulator
-      (loop (operation (first sequence) accumulator) (rest sequence)))))
+  (if (empty? sequence)
+    accumulator
+    (my-fold operation (operation (first sequence) accumulator) (rest sequence))))
 
 (define (my-append sequence1 sequence2)
-  (flatten (cons sequence1 sequence2)))
+  (my-fold cons sequence2 (my-reverse sequence1)))
 
 (define (my-concatenate sequence-of-sequences)
-  (flatten sequence-of-sequences))
+  (my-fold my-append '() (my-reverse sequence-of-sequences)))
