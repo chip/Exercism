df0164e2bd11647d5f8765739a561474e63b167b
Chip Castle <chip@chipcastle.com>
2022-12-16 15:24:13 -0600

    [Racket] Add solution for meetup exercise

--- racket/meetup/meetup-test.rkt
+++ racket/meetup/meetup-test.rkt
@@ -13,27 +13,27 @@
      "Tests for the meetup exercise"
 
      (check-equal? (meetup-day 2013 5 'Monday 'teenth)
-                   (make-date 2013 5 13))))
+                   (make-date 2013 5 13))
 
-     ;(check-equal? (meetup-day 2013 2 'Saturday 'teenth)
-     ;              (make-date 2013 2 16))
+     (check-equal? (meetup-day 2013 2 'Saturday 'teenth)
+                   (make-date 2013 2 16))
 
-     ;(check-equal? (meetup-day 2013 5 'Tuesday 'first)
-     ;              (make-date 2013 5 7))
+     (check-equal? (meetup-day 2013 5 'Tuesday 'first)
+                   (make-date 2013 5 7))
 
-     ;(check-equal? (meetup-day 2013 4 'Monday 'second)
-     ;              (make-date 2013 4 8))
+     (check-equal? (meetup-day 2013 4 'Monday 'second)
+                   (make-date 2013 4 8))
 
-     ;(check-equal? (meetup-day 2013 9 'Thursday 'third)
-     ;              (make-date 2013 9 19))
+     (check-equal? (meetup-day 2013 9 'Thursday 'third)
+                   (make-date 2013 9 19))
 
-     ;(check-equal? (meetup-day 2013 3 'Sunday 'fourth)
-     ;              (make-date 2013 3 24))
+     (check-equal? (meetup-day 2013 3 'Sunday 'fourth)
+                   (make-date 2013 3 24))
 
-     ;(check-equal? (meetup-day 2013 10 'Thursday 'last)
-     ;              (make-date 2013 10 31))
+     (check-equal? (meetup-day 2013 10 'Thursday 'last)
+                   (make-date 2013 10 31))
 
-     ;(check-equal? (meetup-day 2012 2 'Wednesday 'last)
-     ;              (make-date 2012 2 29))))
+     (check-equal? (meetup-day 2012 2 'Wednesday 'last)
+                   (make-date 2012 2 29))))
 
   (run-tests suite))
--- racket/meetup/meetup.rkt
+++ racket/meetup/meetup.rkt
@@ -7,30 +7,18 @@
 (define days-of-week #(Sunday Monday Tuesday Wednesday Thursday Friday Saturday))
 (define teens (sequence->list (in-inclusive-range 13 19)))
 
-(define (make-seconds year month day)
-  (find-seconds 0 0 0 day month year #f))
-
-(define (make-date year month day)
-  (seconds->date (make-seconds day month year #f) #f))
-
 (define (find-last-day year month day)
   (with-handlers ([exn:fail? (lambda (exn) #f)]) 
     (if (find-seconds 0 0 0 day month year #t)
-    ;(if (make-date day month year) ; TODO REVISIT
       day
       #f)))
 
 (define (last-day-of-month year month)
   (last
     (filter-map
-      (lambda (day)
-        ;(printf "try day: ~a\n" day)
-        (find-last-day year month day))
+      (lambda (day) (find-last-day year month day))
       '(28 29 30 31))))
 
-;(printf "ldom: ~a\n" (last-day-of-month 2014 2))
-
-; Then convert 'first, 'second, etc. to analagous racket fns (SPECIAL CASE: 'teenth)
 (define (get-date year month day weekday)
   (let* ([date (seconds->date (find-seconds 0 0 0 day month year #f) #f)]
          [current-weekday (vector-ref days-of-week (date-week-day date))])
@@ -39,7 +27,6 @@
       #f)))
 
 (define (teenth-date? date)
-  #| (printf "teenth-date? date-day ~a\tmember? ~a\tdate: ~a\n" (date-day date) (member (date-day date) teens) date) |#
   (if (member (date-day date) teens)
     date
     #f))
@@ -53,14 +40,14 @@
 (define (find-teenth matching-dates)
   (first (filter-map teenth-date? matching-dates)))
 
-(define (find-match matching-dates fn)
-  (filter-map fn matching-dates))
+(define (find-match matches fn)
+  (cond [(equal? fn 'first) (first matches)]
+        [(equal? fn 'second) (second matches)]
+        [(equal? fn 'third) (third matches)]
+        [(equal? fn 'fourth) (fourth matches)]
+        [else (last matches)]))
 
 (define (meetup-day year month weekday week-of-month)
-  ;(printf "meetup-day: ~a\t~a\t~a\t~a\n" year month weekday week-of-month)
   (let ([matches (matching-days year month weekday week-of-month)])
-    ;(printf "matches: ~a\n" matches)
     (cond [(equal? week-of-month 'teenth) (find-teenth matches)]
           [(find-match matches week-of-month)])))
-#| (check-equal? (meetup-day 2013 5 'Monday 'teenth)) |#
-#| (printf "d: ~a\n" (date->string (make-date 2013 5 13))) |#
