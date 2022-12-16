ff6cf9a527b125713ebb2002281bd4e5acfdd387
Chip Castle <chip@chipcastle.com>
2022-12-16 15:34:58 -0600

    [Racket] Add solution (refactored) for meetup exercise

--- racket/meetup/meetup.rkt
+++ racket/meetup/meetup.rkt
@@ -4,9 +4,6 @@
 
 (require racket/date)
 
-(define days-of-week #(Sunday Monday Tuesday Wednesday Thursday Friday Saturday))
-(define teens (sequence->list (in-inclusive-range 13 19)))
-
 (define (find-last-day year month day)
   (with-handlers ([exn:fail? (lambda (exn) #f)]) 
     (if (find-seconds 0 0 0 day month year #t)
@@ -20,6 +17,7 @@
       '(28 29 30 31))))
 
 (define (get-date year month day weekday)
+  (define days-of-week #(Sunday Monday Tuesday Wednesday Thursday Friday Saturday))
   (let* ([date (seconds->date (find-seconds 0 0 0 day month year #f) #f)]
          [current-weekday (vector-ref days-of-week (date-week-day date))])
     (if (equal? current-weekday weekday)
@@ -27,6 +25,7 @@
       #f)))
 
 (define (teenth-date? date)
+  (define teens (sequence->list (in-inclusive-range 13 19)))
   (if (member (date-day date) teens)
     date
     #f))
@@ -34,20 +33,11 @@
 (define (day-range year month)
   (sequence->list (in-inclusive-range 1 (last-day-of-month year month))))
 
-(define (matching-days year month weekday week-of-month)
-  (filter-map (lambda (day) (get-date year month day weekday)) (day-range year month)))
-
-(define (find-teenth matching-dates)
-  (first (filter-map teenth-date? matching-dates)))
-
-(define (find-match matches fn)
-  (cond [(equal? fn 'first) (first matches)]
-        [(equal? fn 'second) (second matches)]
-        [(equal? fn 'third) (third matches)]
-        [(equal? fn 'fourth) (fourth matches)]
-        [else (last matches)]))
-
 (define (meetup-day year month weekday week-of-month)
-  (let ([matches (matching-days year month weekday week-of-month)])
-    (cond [(equal? week-of-month 'teenth) (find-teenth matches)]
-          [(find-match matches week-of-month)])))
+  (let ([matches (filter-map (lambda (day) (get-date year month day weekday)) (day-range year month))])
+    (cond [(equal? week-of-month 'teenth) (first (filter-map teenth-date? matches))]
+          [(equal? week-of-month 'first) (first matches)]
+          [(equal? week-of-month 'second) (second matches)]
+          [(equal? week-of-month 'third) (third matches)]
+          [(equal? week-of-month 'fourth) (fourth matches)]
+          [else (last matches)])))
