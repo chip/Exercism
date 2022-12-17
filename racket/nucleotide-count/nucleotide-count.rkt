ea8dc63abb37dfd740145a7e5f062ccc18de5837
Chip Castle <chip@chipcastle.com>
2022-12-16 19:59:43 -0600

    [Racket] Add solution (refactored) for nucleotide-count exercise

--- racket/nucleotide-count/nucleotide-count.rkt
+++ racket/nucleotide-count/nucleotide-count.rkt
@@ -3,19 +3,15 @@
 (provide nucleotide-counts)
 
 (define (parse s)
-  (define nucleotides (make-hash))
-  (for ([char (list #\A #\C #\G #\T)])
-    (hash-set! nucleotides char 0))
+  (define nucleotides (make-hash '((#\A . 0) (#\C . 0) (#\G . 0) (#\T . 0))))
   (for ([char (string->list s)])
-    (let ([n (hash-ref nucleotides char)])
-      (hash-set! nucleotides char (add1 n))))
+    (hash-set! nucleotides char (add1 (hash-ref nucleotides char))))
   (for/list ([key (sort (hash-keys nucleotides) char<?)])
     (cons key (hash-ref nucleotides key))))
 
 (define (valid? s)
-  (if (non-empty-string? s)
-    (regexp-match? #rx"^[ACGT]+$" s)
-    #t))
+  (when (non-empty-string? s)
+    (regexp-match? #rx"^[ACGT]+$" s)))
 
 (define (nucleotide-counts s)
   (when (not (valid? s))
