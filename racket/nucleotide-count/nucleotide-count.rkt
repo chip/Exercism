a1d8ea6a804974206839387130f6091690655815
Chip Castle <chip@chipcastle.com>
2022-12-16 18:33:41 -0600

    [Racket] Add WIP (3 passing tests) for nucleotide-count exercise

--- racket/nucleotide-count/nucleotide-count.rkt
+++ racket/nucleotide-count/nucleotide-count.rkt
@@ -2,5 +2,31 @@
 
 (provide nucleotide-counts)
 
-(define (nucleotide-counts nucleotides)
-  (error "Not implemented yet"))
+(define nucleotides (make-hash))
+
+(define (reset-count)
+  (for ([char (list #\A #\C #\G #\T)])
+    (hash-set! nucleotides char 0)))
+
+(define (hash->list)
+  (printf "hash->list\n")
+  (for/list ([key (sort (hash-keys nucleotides) char<?)])
+    (cons key (hash-ref nucleotides key))))
+    
+(define (parse s)
+  (printf "parse\n")
+  (for ([char (string->list s)])
+    (let ([n (hash-ref nucleotides char)])
+      (hash-set! nucleotides char (add1 n))))
+  (hash->list)) 
+
+(define (valid? s)
+  (regexp-match? #rx"^[ACGT]+$" s))
+
+(define (nucleotide-counts s)
+  (reset-count)
+  (if (not (valid? s))
+    (error "invalid nucleotides")
+    (if (non-empty-string? s)
+      (parse s)
+      (hash->list))))
