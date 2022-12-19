c98a9cd189274975371eb7b1e9e6cc55cc6a97f1
Chip Castle <chip@chipcastle.com>
2022-12-19 13:30:16 -0600

    [Racket] Add solution (refactored from community solutions) for raindrops exercise

--- racket/raindrops/raindrops.rkt
+++ racket/raindrops/raindrops.rkt
@@ -2,19 +2,16 @@
 
 (provide convert)
 
-(define h #hash((3 . "Pling") (5 . "Plang") (7 . "Plong")))
-
 (define (drops? n)
+  (define h #hash((3 . "Pling") (5 . "Plang") (7 . "Plong")))
   (string-join
     (filter-map
-      (lambda (m)
-        (if (zero? (modulo n m))
-          (hash-ref h m)
-          #f))
+      (lambda (m) (if (zero? (modulo n m)) (hash-ref h m) #f))
       (hash-keys h))
     ""))
 
 (define (convert n)
-  (if (non-empty-string? (drops? n))
-    (drops? n)
-    (number->string n)))
+  (let ([result (drops? n)])
+    (if (non-empty-string? result)
+      result
+      (number->string n))))
