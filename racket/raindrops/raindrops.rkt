68b5ce399ee992978dfb2579ef5002e3c310d6aa
Chip Castle <chip@chipcastle.com>
2022-12-19 12:41:46 -0600

    [Racket] Add solution for raindrops exercise

--- racket/raindrops/raindrops.rkt
+++ racket/raindrops/raindrops.rkt
@@ -2,5 +2,19 @@
 
 (provide convert)
 
-(define (convert number)
-  (error "Not implemented yet"))
+(define h #hash((3 . "Pling") (5 . "Plang") (7 . "Plong")))
+
+(define (drops? n)
+  (string-join
+    (filter-map
+      (lambda (m)
+        (if (zero? (modulo n m))
+          (hash-ref h m)
+          #f))
+      (hash-keys h))
+    ""))
+
+(define (convert n)
+  (if (non-empty-string? (drops? n))
+    (drops? n)
+    (number->string n)))
