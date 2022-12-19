f4b2f71314224e8ed1fd8c8fccf8b1abcdffe844
Chip Castle <chip@chipcastle.com>
2022-12-19 15:05:42 -0600

    [Racket] Add solution (refactored from community solutions) for rna-transcription exercise

--- racket/rna-transcription/rna-transcription.rkt
+++ racket/rna-transcription/rna-transcription.rkt
@@ -2,9 +2,8 @@
 
 (provide to-rna)
 
+(define (lookup char) 
+  (hash-ref #hash((#\G . "C") (#\C . "G") (#\T . "A") (#\A . "U")) char))
+
 (define (to-rna dna)
-  (string-join
-    (map
-      (lambda (n) (hash-ref #hash(("G" . "C") ("C" . "G") ("T" . "A") ("A" . "U")) n))
-      (map string (string->list dna)))
-    ""))
+  (string-join (map lookup (string->list dna)) ""))
