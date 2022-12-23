3f8737b0c9e3689acbc65298e18002061d93ca7b
Chip Castle <chip@chipcastle.com>
2022-12-23 15:07:32 -0600

    [Racket] Add solution (refactored w/ regexp-match* and px) for word-count exercise

--- racket/word-count/word-count.rkt
+++ racket/word-count/word-count.rkt
@@ -4,21 +4,6 @@
 
 (define (word-count s)
   (define t (make-hash))
-  (for ([w (words s)])
-    (hash-update! t w add1 0))
+  (for ([w (regexp-match* #px"[\\w]+('[\\w]+)?" s)])
+    (hash-update! t (string-downcase w) add1 0))
   t)
-
-(define (words s)
-  (map string-downcase (string-split (sanitize s) #rx" ")))
-
-(define (sanitize s)
-  (string-normalize-spaces (remove-word-quotes (remove-quotes-keep-contractions (remove-puncuation s)))))
-
-(define (remove-puncuation s)
-  (regexp-replace* #rx"[:!&@$%^&.,\"]" s " "))
-
-(define (remove-quotes-keep-contractions s)
-  (regexp-replace* #rx"'([^t])" s "\\1"))
-
-(define (remove-word-quotes s)
-  (regexp-replace* #rx"'(.+)'" s "\\1"))
