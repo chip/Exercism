f67d348074340516de1adf291a1eae5cc3f5e679
Chip Castle <chip@chipcastle.com>
2022-12-23 14:36:44 -0600

    [Racket] Add solution for word-count exercise

--- racket/word-count/word-count.rkt
+++ racket/word-count/word-count.rkt
@@ -2,5 +2,23 @@
 
 (provide word-count)
 
-(define (word-count string)
-  (error "Not implemented yet"))
+(define (word-count s)
+  (define t (make-hash))
+  (for ([w (words s)])
+    (hash-update! t w add1 0))
+  t)
+
+(define (words s)
+  (map string-downcase (string-split (sanitize s) #rx" ")))
+
+(define (sanitize s)
+  (string-normalize-spaces (remove-word-quotes (remove-quotes-keep-contractions (remove-puncuation s)))))
+
+(define (remove-puncuation s)
+  (regexp-replace* #rx"[:!&@$%^&.,\"]" s " "))
+
+(define (remove-quotes-keep-contractions s)
+  (regexp-replace* #rx"'([^t])" s "\\1"))
+
+(define (remove-word-quotes s)
+  (regexp-replace* #rx"'(.+)'" s "\\1"))
