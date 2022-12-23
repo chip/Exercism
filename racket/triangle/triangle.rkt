ad6fd8ba59be4537e03934b91ae5a69b9062b0cf
Chip Castle <chip@chipcastle.com>
2022-12-23 10:07:04 -0600

    [Racket] Add solution for triangle exercise

--- racket/triangle/triangle.rkt
+++ racket/triangle/triangle.rkt
@@ -2,4 +2,16 @@
 
 (provide triangle?)
 
-(define (triangle? sides kind))
+(define (valid? a b c)
+  (and (nor (zero? a) (zero? b) (zero? c))
+       (and (>= (+ a b) c) (>= (+ b c) a) (>= (+ a c) b))))
+
+(define (triangle? sides kind)
+  (let ([a (first sides)]
+        [b (second sides)]
+        [c (third sides)])
+    (if (not (valid? a b c))
+      #f
+      (cond [(symbol=? kind 'isosceles) (or (= a b) (= b c) (= a c))]
+            [(symbol=? kind 'equilateral) (and (= a b) (= b c))]
+            [(symbol=? kind 'scalene) (nor (= a b) (= b c) (= a c))]))))
