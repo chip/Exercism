fad91eff03271e6f68580d739c981320ec774fc2
Chip Castle <chip@chipcastle.com>
2022-12-24 13:25:39 -0600

    [Racket] Add solution for atbash-cipher exercise

--- racket/atbash-cipher/atbash-cipher.rkt
+++ racket/atbash-cipher/atbash-cipher.rkt
@@ -2,8 +2,39 @@
 
 (provide encode decode)
 
+(define plain '(a b c d e f g h i j k l m n o p q r s t u v w x y z))
+(define cipher '(z y x w v u t s r q p o n m l k j i h g f e d c b a))
+
+(define (s->cipher s)
+  (if (regexp-match? #px"[a-z]" s)
+    (symbol->string (list-ref cipher (index-of plain (string->symbol s))))
+    s))
+
+(define (s->plain s)
+  (if (regexp-match? #px"[a-z]" s)
+    (symbol->string (list-ref plain (index-of cipher (string->symbol s))))
+    s))
+
+(define (convert-chunk lst)
+  (map (lambda (w)
+         (map s->cipher (map string (string->list w))))
+      lst))
+
+(define (build-5-letter-strings s)
+  (regexp-match* #px"(.{1,5})" (regexp-replace* #rx"[,. ]" (string-downcase s) "")))
+
+(define (join-chunks lst)
+  (string-append "" (string-join lst "")))
+
 (define (encode m)
-  (error "Not implemented yet"))
+  (string-join (map join-chunks (convert-chunk (build-5-letter-strings m))) " "))
 
 (define (decode m)
-  (error "Not implemented yet"))
+  (let ([s (string-replace m " " "")])
+    (string-join
+      (map (lambda (c)
+             (if (regexp-match? #px"[a-z]" c)
+               (symbol->string (list-ref plain (index-of cipher (string->symbol c))))
+               c))
+           (map string (string->list s)))
+      "")))
