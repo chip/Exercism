93fc53d05f7e96135867214c19f8d35885fda83d
Chip Castle <chip@chipcastle.com>
2023-01-03 16:22:15 -0600

    [Racket] Add passing tests for encode of variable-length-quantity exercise

--- racket/variable-length-quantity/variable-length-quantity-test.rkt
+++ racket/variable-length-quantity/variable-length-quantity-test.rkt
@@ -35,7 +35,7 @@
                       (encode 16383)
                       '(255 127))
 
-        ;;;; 3-byte results
+        ;;; 3-byte results
         (test-equal? "smallest triple byte"
                       (encode 16384)
                       '(129 128 0))
@@ -44,7 +44,7 @@
                       (encode 1048576)
                       '(192 128 0))
 
-        ;;;; 4-byte results
+        ;;; 4-byte results
         (test-equal? "smallest quadruple byte"
                       (encode 2097152)
                       '(129 128 128 0))
@@ -57,7 +57,7 @@
                       (encode 268435455)
                       '(255 255 255 127))
 
-        ;;;; 5-byte results
+        ;;; 5-byte results
         (test-equal? "smallest quintuple byte"
                       (encode 268435456)
                       '(129 128 128 128 0))
@@ -68,20 +68,20 @@
 
         (test-equal? "maximum 32-bit integer input"
                       (encode 4294967295)
-                      '(143 255 255 255 127))))
+                      '(143 255 255 255 127))
 
-        ;;;; Multi-byte tests
-        ;(test-equal? "two single-byte values"
-        ;              (encode 64 127)
-        ;              '(64 127))
+        ;;; Multi-byte tests
+        (test-equal? "two single-byte values"
+                      (encode 64 127)
+                      '(64 127))
 
-        ;(test-equal? "two multi-byte values"
-        ;              (encode 16384 1193046)
-        ;              '(129 128 0 200 232 86))
+        (test-equal? "two multi-byte values"
+                      (encode 16384 1193046)
+                      '(129 128 0 200 232 86))
 
-        ;(test-equal? "many multi-byte values"
-        ;              (encode 8192 1193046 268435455 0 16383 16384)
-        ;              '(192 0 200 232 86 255 255 255 127 0 255 127 129 128 0))))
+        (test-equal? "many multi-byte values"
+                      (encode 8192 1193046 268435455 0 16383 16384)
+                      '(192 0 200 232 86 255 255 255 127 0 255 127 129 128 0))))
 
   (define decode-tests
     (test-suite
