4c35b0069a8cd246c622f8d86f5a6a5fbbec6b0b
Chip Castle <chip@chipcastle.com>
2023-01-06 20:16:14 -0600

    [Racket] Add solution for house exercise

--- racket/house/house.rkt
+++ racket/house/house.rkt
@@ -29,40 +29,25 @@
                         "house that Jack built."))
 
 (define (house [start 1] [end 12])
-  ; TODO remove?
-  (define (determiner-index i)
-    (- (length determiners) i))
-
-  ; TODO remove?
-  (define (postmodifier-index i)
-    (- (length postmodifiers) i))
-
   (define (postmodifier i)
-    (list-ref postmodifiers (postmodifier-index i)))
+    (list-ref postmodifiers (- (length postmodifiers) i)))
 
-  (define (determiner i)
-    ;(displayln (format "determiner i: ~a start: ~a" i start))
-    (if (= i start)
+  (define (determiner i curr)
+    (if (= i curr)
       (list-ref determiners 0)
-      (list-ref determiners (determiner-index i))))
+      (list-ref determiners (- (length determiners) i))))
 
-  ;(displayln (format "start: ~a end: ~a\n" start end))
-  (let loop ([i start]
-             [acc '()])
-    ;(displayln (format "i: ~a acc: ~a\n" i acc))
-    (if (= (length acc) end)
-      (string-join acc "\n")
-      (loop (sub1 i) (append acc (list (string-append (determiner i) " " (postmodifier i))))))))
+  (define (noun-phrases phrases#)
+    (for/fold ([phraselst '()]
+               #:result (list (string-join phraselst "\n")))
+              ([i (range phrases# 0 -1)])
+      (append phraselst (list (string-append (determiner i phrases#) " " (postmodifier i))))))
 
-#| (define phrases '("This is the horse and the hound and the horn")) |#
-#|                   "that belonged to the farmer sowing his corn" |#
-#|                   "that kept the rooster that crowed in the morn" |#
-#|                   "that woke the priest all shaven and shorn" |#
-#|                   "that married the man all tattered and torn" |#
-#|                   "that kissed the maiden all forlorn" |#
-#|                   "that milked the cow with the crumpled horn" |#
-#|                   "that tossed the dog" |#
-#|                   "that worried the cat" |#
-#|                   "that killed the rat" |#
-#|                   "that ate the malt" |#
-#|                   "that lay in the house that Jack built." |#
+  (let loop ([grouplst '()]
+             [i start])
+    (if (> i (length postmodifiers))
+      (string-join grouplst "\n\n")
+      (let ([phraselst (noun-phrases i)])
+        (if (> i end) 
+          (string-join grouplst "\n\n")
+          (loop (append grouplst phraselst) (add1 i)))))))
