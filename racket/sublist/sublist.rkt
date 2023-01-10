947077128d67f797502da1ba3765c68431e51a21
Chip Castle <chip@chipcastle.com>
2023-01-10 15:50:22 -0600

    [Racket] Add WIP for sublist exercise

--- racket/sublist/sublist.rkt
+++ racket/sublist/sublist.rkt
@@ -1,39 +1,50 @@
 #lang racket
 
+;(require seq)
 (provide sublist?)
 
-(define (sublist-found? s l)
-  ;(displayln (format "s ~a l ~a" s l))
-  (if (empty? s)
-    #t
-    (let loop ([l l])
-      (if (empty? l)
-        #f
-        (let* ([ele (first s)]
-               [i (index-of l ele)]
-               [len (length s)])
-          ;(displayln (format "ele ~a i ~a" ele i))
-          (if (not i)
-            #f
-            (if (> len (length l))
-              #f
-              (let ([remaining (drop l i)])
-                (if (> len (length remaining))
-                  #f
-                  (let ([sl (take remaining len)])
-                    (when (equal? s '(1 2 3))
-                      (displayln (format "s ~a l ~a sl ~a" s l sl)))
-                    (if (equal? s sl)
-                      #t
-                      (loop (rest remaining)))))))))))))
+(define (subl? s l)
+  ;(displayln (format "l ~a s ~a" l s))
+  (if (null? l)
+    #f
+    (if (list-prefix? s l)
+      #t
+      (let ([fi (index-of l (first s))]
+            [li (index-of l (last s))])
+        (displayln (format "fi ~a  li ~a  length ~a" fi li (length l)))
+        (if (or (false? fi) (false? li))
+          #f
+          (subl? s (rest l)))))))
+
+;(define (sublist? s l)
+;  ;(displayln (format "s: ~a l: ~a" s l))
+;  (if (equal? s l)
+;    'equal
+;    (if (empty? s)
+;      'sublist
+;      (begin
+;        (if (empty? l)
+;          'superlist
+;          (cond [(and (< (length s) (length l)) (sublist-found? s l)) 'sublist]
+;                [(and (< (length l) (length s)) (sublist-found? l s)) 'superlist]
+;                [else 'unequal]))))))
+;
+;#lang racket
+;
+;(provide sublist?)
+
+(define (_subl? s l)
+  (cond
+    [(null? l) #f]
+    [(list-prefix? s l) #t]
+    [(not (index-of l (first s))) #f]
+    [(not (index-of l (last s))) #f]
+    [else (_subl? s (cdr l))]))
 
 (define (sublist? s l)
-  (if (equal? s l)
-    'equal
-    (if (empty? s)
-      'sublist
-      (if (empty? l)
-        'superlist
-        (cond [(sublist-found? s l) 'sublist]
-              [(sublist-found? l s) 'superlist]
-              [else 'unequal])))))
+  (let ([len (length s)]
+        [len2 (length l)])
+    (cond
+      [(= len len2) (if (equal? s l) 'equal 'unequal)]
+      [(> len len2) (if (subl? l s) 'superlist 'unequal)]
+      [else (if (subl? s l) 'sublist 'unequal)])))
