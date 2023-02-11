ace235261d37aa57ee44f6c67970b679a252b8a3
Chip Castle <chip@chipcastle.com>
2023-02-11 14:57:45 -0600

    WIP for faster alphametics algorithm (part 2)

--- racket/alphametics/alphametics.rkt
+++ racket/alphametics/alphametics.rkt
@@ -1,102 +1,67 @@
 #lang racket
 
-(require profile)
-
 (provide solve)
 
 (define nums (range 10))
-(define nums-len (length nums))
 
 (define (solve puzzle)
   (define words (regexp-split #rx"==|[+]" (string-replace puzzle " " "")))
+  (define lhs (take words (sub1 (length words))))
+  (define rhs (last words))
+  (define letters (remove-duplicates (string->list (string-join words ""))))
+  (define letters-len (length letters))
+  (define first-letters
+    (remove-duplicates (map first (map string->list words))))
 
-  (define letters
-    (for/fold ([acc '()]
-               #:result acc)
-              ([x (string->list (string-join words ""))])
-      (cond
-        [(member x acc) (values acc)]
-        [else (values (append acc (list x)))])))
-
-  (define first-letters (list->set (map (lambda (w) (substring w 0 1)) words)))
-
-  (when (> (length letters) nums-len)
+  (when (> letters-len (length nums))
     '())
 
-  (define (word->sum word h)
-    (string-join
-      (map
-        (lambda (c) (number->string (dict-ref h c)))
-        (string->list word)) ""))
-
-  (define (make-equation o)
-    (for/fold ([acc '()]
-               #:result acc)
-              ([word words])
-      (cond
-        [(member word acc) (values acc)]
-        [else (values (append acc (list (word->sum word o))))])))
-
-  ; TODO necessary to pass h as arg?
-  (define (is-first-letter-of-word-zero? p h)
-    (printf "is-first-letter-of-word-zero p ~a h ~a\n" p h)
-    (ormap
-      (lambda (s)
-        ; (printf "s ~a\n" s)
-        (zero? (dict-ref h s #f)))
+  (define (first-letter-not-zero? lm)
+    (andmap
+      (lambda (c) (not (zero? (dict-ref lm c 'ERROR-first-letter-not-zero))))
       (set->list first-letters)))
 
-  (define (lhs-sum te)
-    ; (printf "lhs-sum te ~a\n" te)
-    (for/sum ([i (map string->number (take te (sub1 (length te))))]) i))
+  (define (list->number lst)
+    (string->number (string-join (map number->string lst) "")))
 
-  (define (solution-found? p)
-    (let ([e (make-equation (map (lambda (s n) (cons s n)) letters p))])
-      ; (printf "solution-found? p ~a e ~a\n" p e)
-      ; (printf "? ~a\n" (= (string->number (last e)) (lhs-sum e)))
-      ; TODO use string=? instead of converting to number?
-      (= (string->number (last e)) (lhs-sum e))))
+  (define (lhs-sum lst)
+    (for/sum ([i (map list->number lst)]) i))
 
-  (define (solution? p h)
-    ; (printf "solution? p ~a h ~a ? ~a\n" p h (and (not (is-first-letter-of-word-zero? p h)) (solution-found? p)))
-    (and (not (is-first-letter-of-word-zero? p h)) (solution-found? p)))
+  (define (eq-sum? e)
+    (let* ([lhs (take e (sub1 (length e)))]
+           [rhs (last e)])
+      (= (lhs-sum lhs) (list->number rhs))))
+
+  (define (map-word w lm)
+    (map (lambda (c) (dict-ref lm c 'ERROR-map-word)) (string->list w)))
+
+  (define (solution-found? lm)
+    (let ([e (map (lambda (w) (map-word w lm)) words)])
+      (eq-sum? e)))
+
+  (define (solution? p)
+    (let ([letter-map (make-hash (map cons letters (stream->list p)))])
+      (and (first-letter-not-zero? letter-map)
+           (solution-found? letter-map))))
 
   (define (generate-permutations items size)
+    (gen-perm items size '()))
+
+  (define (gen-perm items size acc)
     (if (zero? size)
       '(())
-      (for/list ([t (in-list (generate-permutations items (- size 1)))]
+      (for/list ([t (in-list (gen-perm items (- size 1) acc))]
                  #:when #t
                  [i (in-list items)]
                  #:unless (member i t))
-        (cons i t))))
+        (append acc (cons i t)))))
+  
+  (define (build p)
+    (map (lambda (l n) (cons (string l) n)) letters p))
 
-  (define solution
-    (filter-not false?
-      ; (lambda (b) (not (false? b)))
-      (map
-        (lambda (p)
-          ; (printf "map p ~a\n" p)
-          (let ([h (make-hash (map cons letters p))])
-            ; (printf "let h ~a\n" h)
-            ; (printf "map p ~a h ~a\n" p h)
-            (if (solution? p h)
-              (map (lambda (k) (cons (string k) (hash-ref h k))) letters)
-              #f)))
-        (generate-permutations (range 10) (length letters)))))
+  (define result 
+    (for/first ([p (in-list (generate-permutations nums letters-len))]
+                #:when (solution? p))
+      (build p)))
 
-  (let ([r solution])
-    (if (empty? r)
-      r
-      (first r))))
-
-; (define h (make-hash '(("I" . 7) ("B" . 9) ("L" . 3))))
-; (define h (make-hash '((#\I . 7) (#\B . 9) (#\L . 3))))
-; (string-join
-;   (map
-;     (lambda (c) (number->string (dict-ref h c)))
-;     (string->list "BIL")) "") ; => 973
-;(printf "solve 1 result: ~a\n" (solve "AND + A + STRONG + OFFENSE + AS + A + GOOD == DEFENSE"))
-; (printf "solve result: ~a\n" (solve "I + BB == ILL"))
-;              '(("I" . 1)
-;                ("B" . 9)
-;                ("L" . 0)))
+  (if result result '()))
